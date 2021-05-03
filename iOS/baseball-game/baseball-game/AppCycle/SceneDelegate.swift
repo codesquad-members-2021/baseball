508cb5a2e28717f74998b2c5ade682f29f681d88
Song Lee <songlish233@gmail.com>
2021-05-03 16:34:53 +0900

    chore : init xcode project
    - xcode 및 gitignore 생성

---
+++ iOS/baseball-game/baseball-game/AppCycle/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  baseball-game
+//
+//  Created by Song on 2021/05/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ iOS/baseball-game/baseball-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baseball-game
+//
+//  Created by Song on 2021/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
