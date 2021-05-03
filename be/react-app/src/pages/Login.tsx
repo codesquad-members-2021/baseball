e07f62a5e72402393172a549c075adf378f14750
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-03 22:09:22 +0900

    feat: 간단한 로그인 페이지 템플릿 작성

--- be/react-app/src/pages/Login.tsx
+++ be/react-app/src/pages/Login.tsx
@@ -1,4 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-export const Login = () => <Box>로그인 페이지</Box>;
+const useStyles = makeStyles((theme) => ({
+  box: {
+    marginBottom: theme.spacing(4),
+  },
+}));
+
+export const Login = () => {
+  const classes = useStyles();
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleSubmit = () => {
+    alert(`id: ${id}, pw: ${pw}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box className={classes.box}>
+        <TextField
+          label="아이디"
+          variant="outlined"
+          value={id}
+          onChange={(event) => setId(event.target.value)}
+        />
+      </Box>
+      <Box className={classes.box}>
+        <TextField
+          label="비밀번호"
+          variant="outlined"
+          type="password"
+          value={pw}
+          onChange={(event) => setPw(event.target.value)}
+        />
+      </Box>
+      <Box>
+        <Button variant="contained" color="primary" type="submit">
+          로그인
+        </Button>
+      </Box>
+    </form>
+  );
+};
