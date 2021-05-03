7f98314f595cee05333f76baa31bfb518cfaa998
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-03 23:18:28 +0900

    feat: react-google-login 모듈 설치

--- be/react-app/src/pages/OAuth.tsx
+++ be/react-app/src/pages/OAuth.tsx
@@ -1,4 +1,20 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
+import { GoogleLogin } from "react-google-login";
 
-export const OAuth = () => <Box>OAuth 로 로그인 하는 페이지</Box>;
+export const OAuth = () => (
+  <Box>
+    <GoogleLogin
+      clientId="google-login-key"
+      onSuccess={(result) => {
+        alert("로그인 성공");
+        console.log(result);
+      }}
+      onFailure={(result) => {
+        alert("로그인 실패");
+        console.log(result);
+      }}
+      cookiePolicy={"single_host_origin"}
+    />
+  </Box>
+);
