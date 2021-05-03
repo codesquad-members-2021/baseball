bed0cd96af2201dffea2db327fdd1285c4308820
K <PizzaCola.K@gmail.com>
2021-05-03 17:18:32 +0900

    feat: Initialize Project
    - 프로젝트 초기화
    - gitignore.io에서 설정 추가
    
    issue: #1

---
+++ BE/src/main/java/codesquad/team7/baseball/BaseballApplication.java
@@ -0,0 +1,13 @@
+package codesquad.team7.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballApplication.class, args);
+	}
+
+}
