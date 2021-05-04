18d49ae9b11dd271942b4e5f39d8e136660e4fdc
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-04 16:27:59 +0900

    [#12] feat : DB 스키마 스크립트 작성

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,79 @@
+use baseball;
+
+drop table if exists player;
+drop table if exists team;
+drop table if exists game;
+drop table if exists inning;
+drop table if exists history;
+drop table if exists team_participating_in_game;
+drop table if exists player_participating_in_game;
+
+create table player
+(
+    id             int primary key auto_increment,
+    uniform_number int unique  not null,
+    player_name    varchar(20) not null,
+    role           varchar(6),
+    team           int references team (id)
+);
+
+create table team
+(
+    id        int primary key auto_increment,
+    team_name varchar(20) unique not null
+);
+
+create table game
+(
+    id                   int primary key auto_increment,
+    game_title           varchar(30) not null,
+    current_inning       int         not null,
+    isTop                bool        not null,
+    current_strike_count int         not null,
+    current_out_count    int         not null,
+    current_ball_count   int         not null,
+    currentPitcher       int references player (id),
+    currentHitter        int references player (id),
+    isOccupied           bool        not null
+);
+
+create table inning
+(
+    id              int primary key auto_increment,
+    game            int references game (id),
+    inning_number   int not null,
+    home_team_score int not null,
+    away_team_score int not null
+);
+
+create table history
+(
+    id           int primary key auto_increment,
+    inning       int references inning (id),
+    inning_key   int,
+    play_type    varchar(10) not null,
+    strike_count int         not null,
+    ball_count   int         not null,
+    pitcher      int references player (id),
+    hitter       int references player (id)
+);
+
+create table team_participating_in_game
+(
+    id     int primary key auto_increment,
+    team   int references team (id),
+    game   int references game (id),
+    isHome bool not null
+);
+
+create table player_participating_in_game
+(
+    id                int primary key auto_increment,
+    game              int references game (id),
+    player            int references player (id),
+    bat_order         int not null,
+    plate_appearances int not null,
+    hit_count         int not null,
+    out_count         int not null
+);
+
