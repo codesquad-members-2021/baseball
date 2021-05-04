9bf937195e9ee79b356b8670068f1ab84de82067
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-04 16:34:22 +0900

    WIP: 인게임 및 전체 배경 작업중

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Home from './components/Home/Home';
 import Intro from './components/Intro/Intro';
 import InGame from './components/InGame/InGame';
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Router>
+        <GlobalStyle />
         <Switch>
           <Route path="/intro">
             <Intro />
@@ -29,3 +31,19 @@ function App() {
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), url("https://content.api.news/v3/images/bin/85e6759097486d20c75d0f195b0e0913");
+    background-repeat: no-repeat;
+    background-position: center center;
+    min-width: 1280px;
+    width: 1280px;
+    height: 720px;
+    margin: 5rem;
+  }
+
+`;
--- FE/src/components/InGame/InGame.jsx
+++ FE/src/components/InGame/InGame.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const InGame = () => {
   return (
-    <>
-      this is ingame.
-      <button>
-        <Link to="/">메인으로</Link>
-      </button>
-    </>
+    <StyledInGame>
+      <div className="blind"></div>
+    </StyledInGame>
   );
 };
 
 export default InGame;
+
+const StyledInGame = styled.div`
+  width: 100%;
+  height: 100%;
+  .blind {
+    width: 100%;
+    height: 100%;
+    border: red 1px solid;
+  }
+`;
