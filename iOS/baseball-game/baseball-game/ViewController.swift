e4f513c226cfe334c4448c5f290b3375dba3eccc
Song Lee <songlish233@gmail.com>
2021-05-04 11:59:04 +0900

    feat : configure game play screen
    - table view 디자인 확인 위해 ViewController에 임시 테스트 코드 더한 상태
    - core graphics 활용해야 하는 부분 제외하고 구현

--- iOS/baseball-game/baseball-game/ViewController.swift
+++ iOS/baseball-game/baseball-game/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sboTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.sboTableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "sboCell") else { return UITableViewCell()}
+        
+        return cell
+    }
+
+}
