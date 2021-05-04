d7a0d4e4dda7dc53bf36e48810293a5fc4f291e1
dong <ultraraptor@naver.com>
2021-05-04 17:57:26 +0900

    feat : 배포 자동화 스크립트

---
+++ BE_baseball/src/main/resources/deploy.sh
@@ -0,0 +1,45 @@
+# 설정
+LINE='#########################################################'
+SERVADDR='ec2-13-125-229-168.ap-northeast-2.compute.amazonaws.com' #배포할 AWS EC2 서버
+KEYFILE='~/Downloads/DongDong.pem' # 내 개발콤퓨터에서 AWS key 위치
+AUTOINJECT_JAR=1 #1:자동으로 scp명령어로 밀어넣기, #0=안밀어넣고 명령어만 보여주고 끝내기
+#
+echo $LINE
+echo "## DongDong's 바이너리파일 밀어넣기 스크립트 "
+echo "## >>  프로젝트 : baseball  << "
+echo $LINE
+
+cd ../../../
+./gradlew build -x test
+cd build/libs
+BINFILE=`ls | grep *.jar`
+echo $BINFILE
+echo $LINE
+
+if [ ${AUTOINJECT_JAR} -eq 1 ];then
+    scp -i $KEYFILE $BINFILE ubuntu@$SERVADDR:/home/ubuntu
+    echo "JAR FILE SEND OK...!"
+
+    echo ""
+    echo "## tip!"
+    echo ""
+    echo "#how to run?"
+    echo "nohup java -jar ${BINFILE} &"
+    echo ""
+    echo "#how to show log file?"
+    echo "tail -F nohup.out"
+    echo ""
+    echo "#java start fail?"
+    echo "ps -ef | grep java"
+    echo ""
+    echo "go EC2 SERVER"
+    echo $LINE
+
+    ssh -i $KEYFILE ubuntu@$SERVADDR
+fi
+
+
+
+
+
+
