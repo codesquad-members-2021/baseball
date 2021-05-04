f667b0283256d1ec9f9c04c3588b9303dfa45dc9
shimyoungmin <dudals6965@gmail.com>
2021-05-04 11:15:01 +0900

    feat: Create New Project

---
+++ iOS/Baseball/Baseball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baseball
+//
+//  Created by 심영민 on 2021/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ iOS/Baseball/BaseballTests/BaseballTests.swift
@@ -0,0 +1,13 @@
+//
+//  BaseballTests.swift
+//  BaseballTests
+//
+//  Created by 심영민 on 2021/05/04.
+//
+
+import XCTest
+@testable import Baseball
+
+class BaseballTests: XCTestCase {
+
+}
