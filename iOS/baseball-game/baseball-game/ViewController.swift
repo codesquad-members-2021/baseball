5d589da74bf0e3c58fe09dc7521b51381c2dd986
Song Lee <songlish233@gmail.com>
2021-05-04 16:03:29 +0900

    feat : add pitcher select modal (#2)

--- iOS/baseball-game/baseball-game/ViewController.swift
+++ iOS/baseball-game/baseball-game/ViewController.swift
@@ -11,23 +11,31 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var sboTableView: UITableView!
     
+    private let segueId = "selectPitcher"
+    private let cellId = "sboCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.sboTableView.dataSource = self
     }
 
-
+    @IBAction func pitcherChangeTouched(_ sender: Any) {
+        performSegue(withIdentifier: segueId, sender: nil)
+    }
+    
 }
 
+//임시 코드
 extension ViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "sboCell") else { return UITableViewCell()}
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId) else { return UITableViewCell()}
         
         return cell
     }
