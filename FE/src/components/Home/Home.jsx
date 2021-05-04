d651d65240125ec53672554cfb0e209f559ebc2a
junzero741 <junzero741@gmail.com>
2021-05-05 01:58:26 +0900

    feat: OAuth 로그인 버튼 추가

--- FE/src/components/Home/Home.jsx
+++ FE/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { GoogleLogin } from 'react-google-login';
 
 const Home = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -10,13 +11,22 @@ const Home = () => {
     <StyledHome>
       this is home.
       <button>
-        <Link to="/intro">팀 선택하기</Link>
+        <Link to='/intro'>팀 선택하기</Link>
       </button>
       <button onClick={handleLoginClick}>로그인</button>
+      <GoogleLogin
+        clientId='869491322305-t4abjiphkaprn12snvpsqqi1bf8bb3be.apps.googleusercontent.com'
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
     </StyledHome>
   );
 };
 
 export default Home;
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
 const StyledHome = styled.div``;
