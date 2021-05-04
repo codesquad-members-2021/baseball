651d0ad4eccbc4a116a140da97afe151bde1e2e7
1song2 <1song2918@gmail.com>
2021-05-04 16:52:30 +0900

    feat: add game screen UI

---
+++ iOS/BaseballApp/BaseballApp/CurrentPlayerView.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentPlayerView.swift
+//  BaseballApp
+//
+//  Created by Song on 2021/05/04.
+//
+
+import UIKit
+
+class CurrentPlayerView: UIView {
+
+}
---
+++ iOS/BaseballApp/BaseballApp/GroundView.swift
@@ -0,0 +1,12 @@
+//
+//  MatchView.swift
+//  BaseballApp
+//
+//  Created by Song on 2021/05/04.
+//
+
+import UIKit
+
+class GroundView: UIView {
+
+}
