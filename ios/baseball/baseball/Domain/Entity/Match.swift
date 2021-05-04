c4245534696a7b265c24f6f0b93ffad91dbfdb76
zombietux <>
2021-05-04 16:53:06 +0900

    feat: 게임목록 화면 구현 (#9)

---
+++ ios/baseball/baseball/Domain/Entity/Match.swift
@@ -0,0 +1,14 @@
+//
+//  Match.swift
+//  baseball
+//
+//  Created by zombietux on 2021/05/04.
+//
+
+import Foundation
+
+struct Match {
+    private(set) var id: String
+    private(set) var home: String
+    private(set) var away: String
+}
---
+++ ios/baseball/baseball/Presentor/UI/Controller/GameListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  baseball
+//
+//  Created by 이다훈 on 2021/05/04.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let gameList = [
+        Match(id: "1", home: "Captin", away: "Marble"),
+        Match(id: "2", home: "Twins", away: "Tigers"),
+        Match(id: "3", home: "Giants", away: "Dodgers"),
+        Match(id: "4", home: "Heroes", away: "Lions"),
+        Match(id: "5", home: "Heroes", away: "Lions"),
+        Match(id: "6", home: "Heroes", away: "Lions"),
+        Match(id: "7", home: "Heroes", away: "Lions"),
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.dataSource = self
+    }
+}
+
+extension GameListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.gameList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MatchCell.reuseIdentifier, for: indexPath) as? MatchCell else {
+            return UICollectionViewCell()
+        }
+        
+        let match = self.gameList[indexPath.row]
+        cell.configureCell(match)
+        
+        return cell
+    }
+}
+
