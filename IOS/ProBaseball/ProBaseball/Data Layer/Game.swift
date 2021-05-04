c7770979b332f3e6fb1c7f2b09f57bc428a552d2
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-05-04 16:51:41 +0900

    Add: 데이터 모델 추가

---
+++ IOS/ProBaseball/ProBaseball/Data Layer/Game.swift
@@ -0,0 +1,40 @@
+//
+//  Game.swift
+//  ProBaseball
+//
+//  Created by 조중윤 on 2021/05/04.
+//
+
+import Foundation
+
+struct Game {
+    let myTeam: Team
+    let opponentTeam: Team
+    let inning: Inning
+    let ballCount: BallCount
+}
+
+struct Inning {
+    var status: String
+    var inningNum: Int
+}
+
+struct Team {
+    var isAttack: Bool
+    var players: [Player]
+    var currentPitcher: Player
+    var currentBatsman: Player
+}
+
+struct Player {
+    let name: String
+    var plateAppearance: Int
+    var numberOfPitches: Int
+    var hitsNumber: Int
+}
+
+struct BallCount {
+    let strike: Int
+    let ball: Int
+    let out: Int
+}
---
+++ IOS/ProBaseball/ProBaseball/Data Layer/GameList.swift
@@ -0,0 +1,13 @@
+//
+//  GameList.swift
+//  ProBaseball
+//
+//  Created by 조중윤 on 2021/05/04.
+//
+
+import Foundation
+
+struct GameList {
+    let games: [Game]
+}
+
