363c96b63b653ddf938ddaa81276198ad51db827
shimyoungmin <dudals6965@gmail.com>
2021-05-04 14:36:28 +0900

    [#2] feat: Add GameListCell
    게임목록을 표현할 GameList Custom Cell 작성

--- iOS/Baseball/Baseball/ViewController.swift
+++ iOS/Baseball/Baseball/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var gameListCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.gameListCollectionView.delegate = self
+        self.gameListCollectionView.dataSource = self
+        self.gameListCollectionView.register(GameListCell.nib, forCellWithReuseIdentifier: GameListCell.identifier)
     }
-
-
 }
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GameListCell.identifier, for: indexPath) as? GameListCell else {
+            return GameListCell()
+        }
+        return cell
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {}
 
