f7d3e37fdb5a25f1cd3f5b322fb66d298e9fec69
Hannah Park <ha3158987@gmail.com>
2021-05-04 18:23:21 +0900

    feat: MainPage 하위 컴포넌트 생성 - Message.js - GameList.js

--- fe/baseball-fe/src/pages/MainPage/MainPage.js
+++ fe/baseball-fe/src/pages/MainPage/MainPage.js
@@ -1,13 +1,9 @@
 import { useContext } from 'react';
 import { GlobalContext } from 'util/context.js';
 import { GlobalAction } from 'util/action.js';
+import Message from './Message.js';
+import GameList from './GameList.js';
 import styled from 'styled-components';
-
-const StyledMainPage = styled.div`
-  width: 100%;
-  height: 100%;
-  box-shadow: 0 0 0 1px blue inset;
-`;
 
 const testPayload = {
   gameId: 123,
@@ -20,11 +16,31 @@ function MainPage() {
 
   return (
     <StyledMainPage>
-      <button onClick={() => globalDispatch({ type: GlobalAction.SELECT_TEAM, payload: testPayload })}>
-        test
-      </button>
+      <div>BASEBALL GAME ONLINE</div>
+      <Message/>
+      <GameList/>
+      {/* <button onClick={() => globalDispatch({ type: GlobalAction.SELECT_TEAM, payload: testPayload })}>
+        게임시작
+      </button> */}
     </StyledMainPage>
   )
 }
 
 export default MainPage;
+
+const StyledMainPage = styled.div`
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0 0 1px blue inset;
+  background-color: black;
+
+  & > div {
+    color: #fff;
+    font-weight: 600;
+    font-size: 2.5rem;
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
