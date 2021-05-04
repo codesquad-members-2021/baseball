c3c31c0c0b923ee0b7e35f1fb62449f6f7a4ecd2
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-04 18:32:44 +0900

    Feat: Home ViewController setting

---
+++ iOS/baseballApp/baseballApp/Domain/Entity/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  baseballApp
+//
+//  Created by 김지선 on 2021/05/04.
+//
+
+import Foundation
---
+++ iOS/baseballApp/baseballApp/Domain/Entity/Record.swift
@@ -0,0 +1,8 @@
+//
+//  Record.swift
+//  baseballApp
+//
+//  Created by 김지선 on 2021/05/04.
+//
+
+import Foundation
