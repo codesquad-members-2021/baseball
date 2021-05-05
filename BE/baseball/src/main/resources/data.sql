a1062f52365019f1972ece6bd8a1ad45e45a18a9
정이삭 <isaac56@naver.com>
2021-05-05 17:27:48 +0900

    [#2] chore: Add data.sql for importing initial data

---
+++ BE/baseball/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO team (name) VALUES ('코드스쿼드2');
+INSERT INTO player (team_id, name, uniform_number) VALUES (1, '아이작', 1);
+INSERT INTO player (team_id, name, uniform_number) VALUES (1, '쏭', 2);
+INSERT INTO player (team_id, name, uniform_number) VALUES (1, '쑤', 3);
+
+INSERT INTO team (name) VALUES ('마스터즈2');
+INSERT INTO player (team_id, name, uniform_number) VALUES (2, '호눅스', 1);
+INSERT INTO player (team_id, name, uniform_number) VALUES (2, 'JK', 2);
+INSERT INTO player (team_id, name, uniform_number) VALUES (2, '크롱', 3);
