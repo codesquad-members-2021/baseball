e5d010f45397deda97054932dd1c45ea6ea8823c
sanhee <68936@naver.com>
2021-05-05 15:50:44 +0900

    feat: 스프링 부트 프로젝트 생성

---
+++ be/src/test/java/com/team22/baseball/BaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team22.baseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
