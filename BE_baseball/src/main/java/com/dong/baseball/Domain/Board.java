7bb11bf3a01155f48d3ab0833858e02bb3dacf37
dong <ultraraptor@naver.com>
2021-05-05 22:08:45 +0900

    refactor : board 데이터 컬럼 수정

--- BE_baseball/src/main/java/com/dong/baseball/Domain/Board.java
+++ BE_baseball/src/main/java/com/dong/baseball/Domain/Board.java
@@ -5,13 +5,16 @@ import org.springframework.data.annotation.Id;
 public class Board {
 
     @Id
-    private Long gameId;
+    private Long board_Id;
+
+    private Long matchId;
 
     private String turn;
     private int strike;
     private int ball;
     private int out;
-    private int point;
+    private int HomePoint;
+    private int AwayPoint;
     private String pitcher;
     private String batter;
 }
