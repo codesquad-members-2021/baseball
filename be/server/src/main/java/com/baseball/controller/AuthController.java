4a77b65689256aa4678834021b06826cdc334cf5
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-06 08:43:27 +0900

    feat: swagger 문서로 redirect 하는 Controller 작성

--- be/server/src/main/java/com/baseball/controller/AuthController.java
+++ be/server/src/main/java/com/baseball/controller/AuthController.java
@@ -2,11 +2,12 @@ package com.baseball.controller;
 
 import com.baseball.dto.LoginRequest;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +18,12 @@ public class AuthController {
     Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/api/login")
-    public ResponseEntity<String> postLogin(@RequestBody LoginRequest loginRequest) {
+    @ApiOperation(value = "로그인", notes = "아이디와 비밀번호로 로그인합니다.")
+    public ResponseEntity<String> postLogin(@ApiParam("아이디와 비밀번호") @RequestBody LoginRequest loginRequest) {
         logger.debug("{} {}", loginRequest.getId(), loginRequest.getPw());
         if (loginRequest.getId().equals("pyro") && loginRequest.getPw().equals("robin")) {
             return ResponseEntity.ok().body("Login Success!");
         }
         return new ResponseEntity<>("Unauthroized", HttpStatus.UNAUTHORIZED);
-    }
-
-    @GetMapping("/api/ping")
-    public ResponseEntity<String> getPing() {
-        return ResponseEntity.ok().body("pong");
     }
 }
