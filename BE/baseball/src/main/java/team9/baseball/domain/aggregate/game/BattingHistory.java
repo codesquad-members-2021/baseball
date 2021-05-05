2afde10f5d897d918aa431f2716cc1cf4a0cb0d8
정이삭 <isaac56@naver.com>
2021-05-05 17:04:20 +0900

    [#2] feat: Create BattingHistory object that is matched to batting_history db table

---
+++ BE/baseball/src/main/java/team9/baseball/domain/aggregate/game/BattingHistory.java
@@ -0,0 +1,33 @@
+package team9.baseball.domain.aggregate.game;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BattingHistory {
+    @Id
+    private Long id;
+
+    private Integer batterTeamId;
+
+    private Integer batterUniformNumber;
+
+    private int appear;
+
+    private int hits;
+
+    private int out;
+
+    private String keyInGame;
+
+    public BattingHistory(Integer batterTeamId, Integer batterUniformNumber) {
+        this.batterTeamId = batterTeamId;
+        this.batterUniformNumber = batterUniformNumber;
+        this.keyInGame = batterTeamId + "_" + batterUniformNumber;
+    }
+}
