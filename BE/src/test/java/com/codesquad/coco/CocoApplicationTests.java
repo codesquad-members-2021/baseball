79f9857e15b38f671ef063a68e8fd7684d3de432
gisungchoi <chlrltjd159@naver.com>
2021-05-05 14:18:35 +0900

    feat : 초기 세팅

---
+++ BE/src/test/java/com/codesquad/coco/CocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.coco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
