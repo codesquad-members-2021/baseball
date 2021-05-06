d7b33c26c7931a3bd9b2e925c404e1804382a4a2
Jungwon Lee <leejungwon96@gmail.com>
2021-05-06 14:55:17 +0900

    Refactor. 캔버스 유틸 나누기

--- FE/client/src/Components/GamePage/MainContainer/MainLeft/BaseballField.jsx
+++ FE/client/src/Components/GamePage/MainContainer/MainLeft/BaseballField.jsx
@@ -1,61 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-
+import { fitToContainer, drawField } from "utils/canvasUtils";
 const BaseballField = () => {
   let canvasRef = useRef();
-  let canvas;
-  let ctx;
   useEffect(() => {
-    canvas = canvasRef.current;
-    ctx = canvas.getContext("2d");
+    let canvas = canvasRef.current;
+    let ctx = canvas.getContext("2d");
     fitToContainer(canvas);
     drawField(ctx, canvas.offsetWidth, canvas.offsetHeight);
   }, []);
-
-  const drawField = (ctx, x, y) => {
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = "5";
-    ctx.moveTo(350, 70);
-    ctx.lineTo(100, 270);
-    ctx.lineTo(350, 470);
-    ctx.lineTo(600, 270);
-    ctx.lineTo(350, 70);
-    ctx.stroke();
-    drawBase(ctx, 350, 70);
-    drawBase(ctx, 100, 270);
-    drawBase(ctx, 600, 270);
-    drawHomeBase(ctx, 350, 470);
-  };
-
-  const drawBase = (ctx, x, y) => {
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.moveTo(x, y - 30);
-    ctx.lineTo(x - 30, y);
-    ctx.lineTo(x, y + 30);
-    ctx.lineTo(x + 30, y);
-    ctx.lineTo(x, y - 30);
-    ctx.fill();
-  };
-
-  const drawHomeBase = (ctx, x, y) => {
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.moveTo(x, y - 30);
-    ctx.lineTo(x - 30, y);
-    ctx.lineTo(x - 30, y + 50);
-    ctx.lineTo(x + 30, y + 50);
-    ctx.lineTo(x + 30, y);
-    ctx.lineTo(x, y - 30);
-    ctx.fill();
-  };
-
-  function fitToContainer(canvas) {
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-  }
 
   return (
     <BaseballFieldWrapper>
---
+++ FE/client/src/utils/canvasUtils.js
@@ -0,0 +1,65 @@
+const base = {
+  home: {
+    x: 350,
+    y: 470,
+  },
+  first: {
+    x: 600,
+    y: 270,
+  },
+  second: {
+    x: 350,
+    y: 70,
+  },
+  third: {
+    x: 100,
+    y: 270,
+  },
+};
+
+const drawField = (ctx, x, y) => {
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = "5";
+  ctx.moveTo(base.second.x, base.second.y);
+  ctx.lineTo(base.third.x, base.third.y);
+  ctx.lineTo(base.home.x, base.home.y);
+  ctx.lineTo(base.first.x, base.first.y);
+  ctx.lineTo(base.second.x, base.second.y);
+  ctx.stroke();
+  drawBase(ctx, base.second.x, base.second.y);
+  drawBase(ctx, base.third.x, base.third.y);
+  drawBase(ctx, base.first.x, base.first.y);
+  drawHomeBase(ctx, base.home.x, base.home.y);
+};
+
+const drawBase = (ctx, x, y) => {
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.moveTo(x, y - 30);
+  ctx.lineTo(x - 30, y);
+  ctx.lineTo(x, y + 30);
+  ctx.lineTo(x + 30, y);
+  ctx.lineTo(x, y - 30);
+  ctx.fill();
+};
+
+const drawHomeBase = (canvas, x, y) => {
+  canvas.fillStyle = "white";
+  canvas.beginPath();
+  canvas.moveTo(x, y - 30);
+  canvas.lineTo(x - 30, y);
+  canvas.lineTo(x - 30, y + 50);
+  canvas.lineTo(x + 30, y + 50);
+  canvas.lineTo(x + 30, y);
+  canvas.lineTo(x, y - 30);
+  canvas.fill();
+};
+
+const fitToContainer = (canvas) => {
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+};
+
+export { fitToContainer, drawHomeBase, base, drawBase, drawField };
