b240b9458058870ee879dffdaeb5d772eab99c4d
gisungchoi <chlrltjd159@naver.com>
2021-05-06 18:00:46 +0900

    chore : 패키지 이동
    - 패키지 이동 import 변경

--- BE/src/main/java/com/codesquad/coco/game/domain/GameRepository.java
+++ BE/src/main/java/com/codesquad/coco/game/domain/GameRepository.java
@@ -1,5 +1,6 @@
 package com.codesquad.coco.game.domain;
 
+import com.codesquad.coco.game.domain.model.Game;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GameRepository extends CrudRepository<Game, Long> {
