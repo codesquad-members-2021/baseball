65e47c8d180ba7123d812c924286880693bfd393
gisungchoi <chlrltjd159@naver.com>
2021-05-06 23:20:04 +0900

    feat : DTOConverter 정의
    - team의 정보를 변환하는 converter 정의

---
+++ BE/src/main/java/com/codesquad/coco/utils/DTOConverter.java
@@ -0,0 +1,18 @@
+package com.codesquad.coco.utils;
+
+import com.codesquad.coco.game.domain.model.PlayerDTO;
+import com.codesquad.coco.team.domain.DTO.TeamDTO;
+import com.codesquad.coco.team.domain.Team;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DTOConverter {
+
+    public static TeamDTO teamToDTO(Team team) {
+        List<PlayerDTO> opponentPlayerDTO = team.getPlayers().stream()
+                .map(player -> new PlayerDTO(player.getId(), player.getName(), player.getType()))
+                .collect(Collectors.toList());
+        return new TeamDTO(team.getName(), opponentPlayerDTO);
+    }
+}
