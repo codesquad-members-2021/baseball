ee680b760b1b72e5ffe4406659d90a65c012a43a
Jungwon Lee <leejungwon96@gmail.com>
2021-05-06 14:55:17 +0900

    Refactor. 캔버스 유틸 나누기

--- FE/client/src/utils/canvasUtils.js
+++ FE/client/src/utils/canvasUtils.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const drawField = (ctx, x, y) => {
   ctx.strokeStyle = "white";
   ctx.lineWidth = "5";
@@ -11,6 +12,40 @@ const drawField = (ctx, x, y) => {
   drawBase(ctx, x / 10, y / 2);
   drawBase(ctx, x - x / 10, y / 2);
   drawHomeBase(ctx, x / 2, y - y / 12);
+=======
+const base = {
+  home: {
+    x: 350,
+    y: 470,
+  },
+  first: {
+    x: 600,
+    y: 270,
+  },
+  second: {
+    x: 350,
+    y: 70,
+  },
+  third: {
+    x: 100,
+    y: 270,
+  },
+};
+
+const drawField = (ctx, x, y) => {
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = "5";
+  ctx.moveTo(base.second.x, base.second.y);
+  ctx.lineTo(base.third.x, base.third.y);
+  ctx.lineTo(base.home.x, base.home.y);
+  ctx.lineTo(base.first.x, base.first.y);
+  ctx.lineTo(base.second.x, base.second.y);
+  ctx.stroke();
+  drawBase(ctx, base.second.x, base.second.y);
+  drawBase(ctx, base.third.x, base.third.y);
+  drawBase(ctx, base.first.x, base.first.y);
+  drawHomeBase(ctx, base.home.x, base.home.y);
+>>>>>>> d7b33c2... Refactor. 캔버스 유틸 나누기
 };
 
 const drawBase = (ctx, x, y) => {
@@ -42,4 +77,9 @@ const fitToContainer = (canvas) => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 };
+<<<<<<< HEAD
 export { fitToContainer, drawHomeBase, drawBase, drawField };
+=======
+
+export { fitToContainer, drawHomeBase, base, drawBase, drawField };
+>>>>>>> d7b33c2... Refactor. 캔버스 유틸 나누기
