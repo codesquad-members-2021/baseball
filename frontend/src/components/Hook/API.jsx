d98ff0cc7761a1ba53a58f468a5cb8872156833e
Seohyoung <prophecy803@gmail.com>
2021-05-06 11:08:32 +0900

    [#4] feat : startPage UI 진행 중
    - Start Page 메인에서 데이터 패치 처리
    - Team List 컴포넌트 내 스타일 작업 중

--- frontend/src/components/Hook/API.jsx
+++ frontend/src/components/Hook/API.jsx
@@ -1,16 +1,16 @@
 let url = 'https://890e124d-c97a-4c91-a91a-0d19d727c1d6.mock.pstmn.io';
 const API = {
-	get: {
-		teamList: async () => {
-			const response = await fetch(`${url}/games`);
-			return response.json();
-		},
-	},
-	post: {},
+  get: {
+    teamList: async () => {
+      const response = await fetch(`${url}/games`);
+      return response.json();
+    },
+  },
+  post: {},
 };
 
 export default API;
 
-// fetch("https://example.com")
+// fetch(url)
 //     .then(response => response.json())
 //     .then(jsondata => console.log(jsondata))
--- frontend/src/components/StartPage/index.jsx
+++ frontend/src/components/StartPage/index.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import TeamList from './TeamList';
-// import { theme } from './/Style/Theme';
+import { theme } from '../Style/Theme';
+
+const TitleStyle = styled.div`
+  font-size: ${theme.fontSize.XX_large};
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.white};
+`;
+
+const MessageStyle = styled.div`
+  font-size: ${theme.fontSize.large};
+  font-weight: ${theme.fontWeight.normal};
+  color: ${theme.colors.white};
+`;
+
+const GameTitle = () => {
+  return <TitleStyle>BASEBALL GAME ONLINE</TitleStyle>;
+};
+
+const MainMessage = () => {
+  return <MessageStyle>참가할 게임을 선택하세요!</MessageStyle>;
+};
 
 const StartPage = () => {
-	return <TeamList></TeamList>;
+  return (
+    <>
+      <GameTitle />
+      <MainMessage />
+      <TeamList></TeamList>
+    </>
+  );
 };
 
 export default StartPage;
