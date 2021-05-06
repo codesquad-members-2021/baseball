11bfeebd9f5c2225af38da49e2b57bf38821d420
Jungwon Lee <leejungwon96@gmail.com>
2021-05-06 15:07:14 +0900

    Feat. PITCH버튼 만들고 배치

--- FE/client/src/Components/GamePage/MainContainer/MainLeft/BaseballField.jsx
+++ FE/client/src/Components/GamePage/MainContainer/MainLeft/BaseballField.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { fitToContainer, drawField } from "utils/canvasUtils";
+import PitchButton from "Components/GamePage/MainContainer/MainLeft/PitchButton";
+
 const BaseballField = () => {
   let canvasRef = useRef();
   useEffect(() => {
@@ -13,6 +15,7 @@ const BaseballField = () => {
   return (
     <BaseballFieldWrapper>
       <canvas ref={canvasRef} />
+      <PitchButton />
     </BaseballFieldWrapper>
   );
 };
@@ -21,6 +24,7 @@ const BaseballFieldWrapper = styled.div`
   color: white;
   width: 65%;
   height: 100%;
+  position: relative;
 `;
 
 export default BaseballField;
--- FE/client/src/Components/GamePage/MainContainer/MainLeft/PitchButton.jsx
+++ FE/client/src/Components/GamePage/MainContainer/MainLeft/PitchButton.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 const PitchButton = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <PitchBtn>PITCH</PitchBtn>
+    </>
   );
 };
+
+const PitchBtn = styled.button`
+  position: absolute;
+  left: 37%;
+  top: 42%;
+  background: none;
+  color: white;
+  font-size: 1.5rem;
+  border: 0.1rem solid white;
+  border-radius: 10px;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  &:hover {
+    color: black;
+    background: white;
+  }
+`;
 
 export default PitchButton;
