98e7ccd7add387764d89ab404c21d243f8312ba5
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-06 17:14:22 +0900

    Feat: add TeamRecordsDTO for showing records including teamId

---
+++ BE/src/main/java/baseball/service/dto/TeamRecordsDTO.java
@@ -0,0 +1,25 @@
+package baseball.service.dto;
+
+import baseball.domain.Record;
+import baseball.domain.RecordMember;
+
+import java.util.Set;
+
+public class TeamRecordsDTO {
+
+    private Long teamId;
+    private Set<RecordMember> records;
+
+    public TeamRecordsDTO(Long teamId, Set<RecordMember> records) {
+        this.teamId = teamId;
+        this.records = records;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public Set<RecordMember> getRecords() {
+        return records;
+    }
+}
