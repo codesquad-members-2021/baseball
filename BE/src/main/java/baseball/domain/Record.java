55b4ae1e44f73d222a7e0a7a800093e5314cd977
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-06 17:09:08 +0900

    Feat: add Record domain class for record table

---
+++ BE/src/main/java/baseball/domain/Record.java
@@ -0,0 +1,36 @@
+package baseball.domain;
+
+public class Record {
+
+    private int atBat;
+    private int hit;
+    private int out;
+    private double average;
+
+    public Record(int atBat, int hit, int out) {
+        this.atBat = atBat;
+        this.hit = hit;
+        this.out = out;
+        this.average = calculateAverage(atBat, hit);
+    }
+
+    private double calculateAverage(int atBat, int hit) {
+        return atBat / (double) hit;
+    }
+
+    public int getAtBat() {
+        return atBat;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public int getOut() {
+        return out;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+}
