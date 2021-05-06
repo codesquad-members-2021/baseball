e816ae3d81cd5a78555e605b77f4bcb80afca3dc
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-07 00:53:09 +0900

    Feat: move business logic to TeamService

--- BE/src/main/java/baseball/controller/TeamController.java
+++ BE/src/main/java/baseball/controller/TeamController.java
@@ -1,63 +1,39 @@
 package baseball.controller;
 
-import baseball.domain.Member;
-import baseball.domain.Record;
-import baseball.domain.RecordMember;
-import baseball.domain.Team;
-import baseball.repository.TeamRepository;
+import baseball.service.TeamService;
 import baseball.service.dto.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/teams")
 public class TeamController {
 
-    private TeamRepository teamRepository;
+    private TeamService teamService;
 
-    public TeamController(TeamRepository teamRepository) {
-        this.teamRepository = teamRepository;
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
     }
 
-    @PostMapping("/teams")
-    public void createTeams(@RequestBody List<RequestTeamDTO> teamDTOS) {
-        List<Team> teams = teamDTOS.stream()
-                .map(RequestTeamDTO::toTeam)
-                .collect(Collectors.toList());
-        teamRepository.saveAll(teams);
+    @PostMapping
+    public void createTeams(@RequestBody List<RequestTeamDTO> teamDTOs) {
+        teamService.saveTeams(teamDTOs);
     }
 
-    @GetMapping("/teams")
+    @GetMapping
     public List<TeamDTO> showTeams() {
-        Iterable<Team> teams = teamRepository.findAll();
-
-        List<TeamDTO> teamDTOS = new ArrayList<>();
-        for (Team team : teams) {
-            List<MemberDTO> memberDTOS = team.getMembers().stream()
-                    .map(MemberDTO::toMemberDTO)
-                    .collect(Collectors.toList());
-            teamDTOS.add(new TeamDTO(team, memberDTOS));
-        }
-        return teamDTOS;
+        return teamService.convertToTeamDTOList();
     }
 
-    @GetMapping("/teams/{teamId}/records")
+    @PostMapping("/{teamId}/{memberId}/record")
+    public void addRecord(@PathVariable Long teamId, @PathVariable Long memberId,
+                          int atBat, int hit, int out) {
+        teamService.insertRecord(teamId, memberId, atBat, hit, out);
+    }
+
+    @GetMapping("/{teamId}/records")
     public TeamRecordsDTO showRecords(@PathVariable Long teamId) {
-        Team team = teamRepository.findById(teamId).orElseThrow(NoSuchElementException::new);
-        Set<Member> members = team.getMembers();
-        for (Member member : members) {
-            Record record = new Record(3, 1, 1);
-            member.setRecord(record);
-        }
-        teamRepository.save(team);
-
-        Set<RecordMember> recordMembers = new HashSet<>();
-        for (Member member : members) {
-            RecordMember recordMember = teamRepository.findRecordByMemberId(member.getId()).get();
-            recordMembers.add(recordMember);
-        }
-
-        return new TeamRecordsDTO(teamId, recordMembers);
+        return teamService.getRecordsOfTeam(teamId);
     }
 }
