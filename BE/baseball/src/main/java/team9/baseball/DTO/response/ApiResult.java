91025c8fcaac1bd6d13cc8cefe80d45ace799698
정이삭 <isaac56@naver.com>
2021-05-07 01:43:53 +0900

    [#6] feat: Create common response DTO
    response에 사용될 공통 형식인 ApiResult 클래스 생성

---
+++ BE/baseball/src/main/java/team9/baseball/DTO/response/ApiResult.java
@@ -0,0 +1,36 @@
+package team9.baseball.DTO.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResult<T> {
+    private T data;
+    private String error;
+
+    private ApiResult(T data, String error) {
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> ApiResult<T> succeed(T data) {
+        return new ApiResult(data, null);
+    }
+
+    public static ApiResult<?> failed(String errorMessage) {
+        return new ApiResult<>(null, errorMessage);
+    }
+
+    public static ApiResult<?> failed(Throwable throwable) {
+        return failed(throwable.getMessage());
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
