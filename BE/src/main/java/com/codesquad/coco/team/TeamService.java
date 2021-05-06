c719107938c46e44306a92030746cdb912adc034
gisungchoi <chlrltjd159@naver.com>
2021-05-06 23:24:58 +0900

    feat : TeamService 정의
    - 사용자가 home,away를 고름에 따라 다른 동작을 한다
    - 다른 동작이라 함은 Game에서 home과 away를 조정하는 동작이다

--- BE/src/main/java/com/codesquad/coco/team/TeamService.java
+++ BE/src/main/java/com/codesquad/coco/team/TeamService.java
@@ -1,6 +1,11 @@
 package com.codesquad.coco.team;
 
-import com.codesquad.coco.team.domain.MainTeamDTO;
+import com.codesquad.coco.game.domain.DAO.GameDAO;
+import com.codesquad.coco.game.domain.model.Game;
+import com.codesquad.coco.player.domain.PlayerDAO;
+import com.codesquad.coco.team.domain.DTO.MainPageTeamDTO;
+import com.codesquad.coco.team.domain.DTO.TeamChoiceDTO;
+import com.codesquad.coco.team.domain.Team;
 import com.codesquad.coco.team.domain.TeamDAO;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +13,42 @@ import org.springframework.stereotype.Service;
 public class TeamService {
 
     private final TeamDAO teamDAO;
+    private final GameDAO gameDAO;
+    private final PlayerDAO playerDAO;
 
-    public TeamService(TeamDAO teamDAO) {
+    public TeamService(TeamDAO teamDAO, GameDAO gameDAO, PlayerDAO playerDAO) {
         this.teamDAO = teamDAO;
+        this.gameDAO = gameDAO;
+        this.playerDAO = playerDAO;
     }
 
-    public MainTeamDTO findMainTeams() {
+    public MainPageTeamDTO findMainTeams() {
         return teamDAO.findAllName();
+    }
+
+    public Long makeHomeGame(TeamChoiceDTO choiceDTO) {
+        Team playerTeam = getPlayerTeam(choiceDTO);
+        Team opponentTeam = getOpponentTeam(choiceDTO);
+
+        Game game = new Game(opponentTeam, playerTeam, "home");
+        return gameDAO.save(game);
+    }
+
+    public Long makeAwayGame(TeamChoiceDTO choiceDTO) {
+        Team playerTeam = getPlayerTeam(choiceDTO);
+        Team opponentTeam = getOpponentTeam(choiceDTO);
+
+        Game game = new Game(playerTeam, opponentTeam, "away");
+        return gameDAO.save(game);
+    }
+
+    private Team getPlayerTeam(TeamChoiceDTO choiceDTO) {
+        String teamName = choiceDTO.getPlayer();
+        return new Team(teamName, playerDAO.findByTeamName(teamName));
+    }
+
+    private Team getOpponentTeam(TeamChoiceDTO choiceDTO) {
+        String teamName = choiceDTO.getOpponent();
+        return new Team(teamName, playerDAO.findByTeamName(teamName));
     }
 }
