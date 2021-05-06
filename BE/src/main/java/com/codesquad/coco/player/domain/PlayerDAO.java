bcefb9378d60d3488ded9ffa8e08e1954f8c6c54
gisungchoi <chlrltjd159@naver.com>
2021-05-07 00:30:31 +0900

    feat : playerDAO 기능 추가
    - 레코드 업데이트와 선수 찾기 추가

--- BE/src/main/java/com/codesquad/coco/player/domain/PlayerDAO.java
+++ BE/src/main/java/com/codesquad/coco/player/domain/PlayerDAO.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,6 @@ public class PlayerDAO {
     }
 
     public List<Player> findByTeamName(String teamName) {
-        // todo :  선수 순서 보장
         String sql = "SELECT p.id as pid, p.type, p.name, r.id as rid, r.outs, r.hits, r.at_bat, r.average " +
                 "FROM player p " +
                 "inner JOIN record r ON r.player = p.id " +
@@ -44,4 +44,43 @@ public class PlayerDAO {
         return players;
     }
 
+    public Player findById(Long id) {
+        String sql = "select p.id as pid, p.name, p.type, r.id as rid, r.outs, r.hits, r.at_bat, r.average " +
+                "from player p " +
+                "inner JOIN record r ON r.player = p.id " +
+                "where p.id= " + id;
+
+        List<Player> query = template.query(sql, (rs, rowNum) -> {
+            Record record = new Record(
+                    rs.getLong("rid"),
+                    rs.getInt("at_bat"),
+                    rs.getInt("hits"),
+                    rs.getInt("outs"),
+                    rs.getDouble("average")
+            );
+            Player player = new Player(
+                    rs.getLong("pid"),
+                    rs.getString("name"),
+                    rs.getString("type"),
+                    record
+            );
+            return player;
+        });
+
+        return query.get(0);
+    }
+
+    public void updateRecord(Record record) {
+        String sql = "update record set at_bat = ?, hits = ?, outs = ?, average = ? where id = ?";
+
+        template.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, record.getAtBat());
+            ps.setInt(2, record.getHits());
+            ps.setInt(3, record.getOuts());
+            ps.setDouble(4, record.getAverage());
+            ps.setLong(5, record.getId());
+            return ps;
+        });
+    }
 }
