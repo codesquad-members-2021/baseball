accbdaffdf91a3d80eaf53d63ce3719aa0d91f30
Jungwon Lee <leejungwon96@gmail.com>
2021-05-06 15:26:43 +0900

    Feat. Home 페이지 scroll기능 추가

--- FE/client/src/Components/Home/GameList/Game.jsx
+++ FE/client/src/Components/Home/GameList/Game.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Team from "Components/Home/GameList/Team";
 
-
 const Game = () => {
   return (
     <GameBox>
@@ -17,32 +16,32 @@ const Game = () => {
 };
 
 const GameBox = styled.li`
-  display:flex;
-  flex-direction:column;
-  width:31.25rem;
-  height: 3.75rem;
-  background:white;
-  color:black;
+  display: flex;
+  flex-direction: column;
+  width: 31.25rem;
+  height: 4.5rem;
+  background: white;
+  color: black;
   opacity: 0.7;
-  padding:0.6rem 1.7rem;
-  border-radius:1.7rem;
-  font-size:1.3rem;
+  padding: 0.6rem 1.7rem;
+  border-radius: 1rem;
+  font-size: 1.3rem;
 `;
 
 const GameNumber = styled.span`
-  color:red;
-  font-size:0.9rem;
+  color: red;
+  font-size: 0.9rem;
   font-weight: 700;
 `;
 
 const TeamDiv = styled.div`
-  display:flex;
-  justify-content:space-around;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const VsSpan = styled.span`
-  color: #48474C;
-  font-weight:700;
+  color: #48474c;
+  font-weight: 700;
 `;
 
 export default Game;
--- FE/client/src/Components/Home/GameList/index.jsx
+++ FE/client/src/Components/Home/GameList/index.jsx
@@ -2,27 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import Game from "Components/Home/GameList/Game";
 
-
 const GameList = () => {
-
-
-  const TeamData = Array.from({ length: 3 });
+  const TeamData = Array.from({ length: 7 });
   return (
     <GameBoxList>
-      {TeamData.map(team => {
-        return <Game />
+      {TeamData.map((team) => {
+        return <Game />;
       })}
     </GameBoxList>
   );
 };
 
 const GameBoxList = styled.ul`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
   place-items: center;
-  margin:2rem auto;
-  gap:1rem;
-
+  margin: 2rem auto;
+  max-height: 18rem;
+  gap: 1rem;
+  width: 33rem;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default GameList;
