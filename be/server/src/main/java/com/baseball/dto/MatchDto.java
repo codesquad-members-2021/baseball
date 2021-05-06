e9ec9b17cfa7c92c7330e943479e26b05799f71f
고정완 (Can) <jw.gho@ecubelabs.com>
2021-05-06 16:07:38 +0900

    Add domain/Match
    기존의 MatchDto 에 대해서 Match 도메인과 레포지토리를 작성하였다.
    Service 는 로직 없이 순수하게 domain 을 dto 로 바꿔주는 역할만을 해야한다.
    진정한 비즈니스 로직은 domain 에서 이루어져야한다.

--- be/server/src/main/java/com/baseball/dto/MatchDto.java
+++ be/server/src/main/java/com/baseball/dto/MatchDto.java
@@ -1,15 +1,41 @@
 package com.baseball.dto;
 
-public class MatchDto {
+import com.baseball.domain.Match;
 
+public class MatchDto {
     private final String home;
     private final String away;
     private final String id;
 
-    public MatchDto(String home, String away, String id) {
-        this.home = home;
-        this.away = away;
-        this.id = id;
+    private MatchDto(Builder builder) {
+        this.home = builder.home;
+        this.away = builder.away;
+        this.id = builder.id;
+    }
+
+    private static class Builder {
+        private String home;
+        private String away;
+        private String id;
+
+        private Builder home(String home) {
+            this.home = home;
+            return this;
+        }
+
+        private Builder away(String away) {
+            this.away = away;
+            return this;
+        }
+
+        private Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        private MatchDto build() {
+            return new MatchDto(this);
+        }
     }
 
     public String getHome() {
@@ -23,4 +49,13 @@ public class MatchDto {
     public String getId() {
         return id;
     }
+
+    public static MatchDto from(Match match) {
+        Builder builder = new Builder()
+                .home(match.getHome())
+                .away(match.getAway())
+                .id(match.getId());
+        return builder.build();
+    }
+
 }
--- be/server/src/main/java/com/baseball/service/BaseballService.java
+++ be/server/src/main/java/com/baseball/service/BaseballService.java
@@ -1,21 +1,31 @@
 package com.baseball.service;
 
 import com.baseball.dto.*;
+import com.baseball.repository.MatchRepository;
+import com.baseball.repository.MockedMatchRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BaseballService {
+    private final MatchRepository matchRepository;
+
+    // FIXME: 나중에 JDBC 연결을 하면 이 생성자는 삭제해야함
+    public BaseballService() {
+        this(new MockedMatchRepository());
+    }
+
+    public BaseballService(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
 
     public List<MatchDto> getMatches() {
-        List<MatchDto> matches = new ArrayList<>();
-        matches.add(new MatchDto("Captain", "Marble", "U924AX"));
-        matches.add(new MatchDto("Honux", "Crong", "H132UY"));
-        matches.add(new MatchDto("Android", "Apple", "M887UW"));
-        return matches;
+        return matchRepository.findAll().stream()
+                .map(MatchDto::from)
+                .collect(Collectors.toList());
     }
 
     public MatchInfoDto getProgress(String id) {
