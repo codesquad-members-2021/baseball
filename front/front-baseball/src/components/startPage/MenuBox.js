18256781cb1d60083a6f80be15bac776126468ea
jjunyjjuny <jjuny546@gmail.com>
2021-05-06 21:03:14 +0900

    feat : dummyFetch test

--- front/front-baseball/src/components/startPage/MenuBox.js
+++ front/front-baseball/src/components/startPage/MenuBox.js
@@ -1,20 +1,79 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import styled, { css } from "styled-components";
 import { MESSAGE } from "../../utils/constant.js";
 import Game from "./Game";
 const MenuBox = () => {
-  const testArray = [1, 2, 3, 4, 5, 6];
-  useEffect(() => {});
+  const [games, setGames] = useState([1, 2, 3]);
+  const scrollHeight = useRef(0);
+  useEffect(() => {
+    const fetchGamesDate = async () => {
+      const newGames = await dummyFetch();
+      setGames((games) => [...games, ...newGames]);
+    };
+    if (games.length < 10) fetchGamesDate();
+  });
   return (
     <MenuWrapper>
       <MessageBox>{MESSAGE.INIT}</MessageBox>
       <Games>
-        {testArray.map((el) => (
+        {games.map((el) => (
           <Game />
         ))}
       </Games>
     </MenuWrapper>
   );
+};
+
+const dummyFetch = () => {
+  const data = {
+    games: [
+      {
+        gameId: 1,
+        home: {
+          teamId: "team-1",
+          teamName: "Captin",
+          selected: false,
+        },
+        away: {
+          teamId: "team-2",
+          teamName: "Mavel",
+          selected: true,
+        },
+      },
+      {
+        gameId: 2,
+        home: {
+          teamId: "team-3",
+          teamName: "Twins",
+          selected: false,
+        },
+        away: {
+          teamId: "team-4",
+          teamName: "Tigers",
+          selected: true,
+        },
+      },
+      {
+        gameId: 3,
+        home: {
+          teamId: "team-5",
+          teamName: "Rockets",
+          selected: false,
+        },
+        away: {
+          teamId: "team-6",
+          teamName: "Dodgers",
+          selected: true,
+        },
+      },
+    ],
+  };
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data.games);
+    }, 3000);
+  });
 };
 const MenuWrapper = styled.div`
   margin-top: 1rem;
