4d6e0c2f88941587a09d2949dbe9a5390a7949a9
gisungchoi <chlrltjd159@naver.com>
2021-05-06 18:03:24 +0900

    feat : Game svae, findById 변경
    - game을 세이브할때 점수판도 같이 저장한다
    - game저장 후 id를 받아서 점수판을 저장
    - find시 점수판도 함께 가져온다
    - 점수판은 이닝을 같이 가져온다

--- BE/src/main/java/com/codesquad/coco/game/domain/GameDAO.java
+++ BE/src/main/java/com/codesquad/coco/game/domain/GameDAO.java
@@ -1,11 +1,20 @@
 package com.codesquad.coco.game.domain;
 
+import com.codesquad.coco.game.domain.model.Game;
+import com.codesquad.coco.game.domain.model.ScoreBoard;
 import com.codesquad.coco.player.domain.PlayerDAO;
 import com.codesquad.coco.team.domain.Team;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 @Component
@@ -13,16 +22,44 @@ public class GameDAO {
 
     private JdbcTemplate template;
     private PlayerDAO playerDAO;
+    private ScoreBoardDAO boardDAO;
 
-    public GameDAO(DataSource dataSource, PlayerDAO playerDAO) {
+    public GameDAO(DataSource dataSource, PlayerDAO playerDAO, ScoreBoardDAO boardDAO) {
         this.template = new JdbcTemplate(dataSource);
         this.playerDAO = playerDAO;
+        this.boardDAO = boardDAO;
     }
 
-    public void save(Game game) {
-        String sql = "insert into game (home,away,user_type) values (?, ?,?)";
-        template.update(sql, game.awayTeamName(), game.homeTeamName(), game.getUserType());
+    public Long save(Game game) {
+        Long gameId = saveGame(game);
+        ScoreBoard away = new ScoreBoard(gameId, game.awayTeamName());
+        ScoreBoard home = new ScoreBoard(gameId, game.homeTeamName());
+        saveScoreBoard(game, away);
+        saveScoreBoard(game, home);
+        return gameId;
     }
+
+    private Long saveScoreBoard(Game game, ScoreBoard away) {
+        Long scoreId = boardDAO.save(away);
+        game.addScoreBoard(away);
+        return scoreId;
+    }
+
+    private Long saveGame(Game game) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "insert into game (home,away,user_type) values (?, ?, ?)";
+
+        template.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, game.homeTeamName());
+            ps.setString(2, game.awayTeamName());
+            ps.setString(3, game.getUserType());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
 
     public Game findById(Long id) {
         String sql = "select g.id, g.home,g.away,g.user_type from game g where g.id = " + id;
@@ -30,16 +67,24 @@ public class GameDAO {
         List<Game> query = template.query(sql, (rs, rowNum) -> {
             String homeTeamName = rs.getString("home");
             String awayTeamName = rs.getString("away");
+
+            ScoreBoard homeScoreBoard = boardDAO.findByGameIdAndTeamName(id, homeTeamName);
+            ScoreBoard awayScoreBoard = boardDAO.findByGameIdAndTeamName(id, awayTeamName);
+
             Team homeTeam = new Team(
                     homeTeamName, playerDAO.findByTeamName(homeTeamName)
             );
             Team awayTeam = new Team(
                     awayTeamName, playerDAO.findByTeamName(awayTeamName)
             );
+
+
             Game game = new Game(
                     rs.getLong("id"),
                     homeTeam,
                     awayTeam,
+                    Collections.unmodifiableSet(new HashSet<ScoreBoard>(
+                            Arrays.asList(homeScoreBoard, awayScoreBoard))),
                     rs.getString("user_type")
             );
 
