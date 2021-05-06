876becd670a32f1d1004ca405ede2358fb70d8c4
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-06 17:16:36 +0900

    Feat: update schema.sql

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,7 +1,15 @@
+drop table if exists game;
 drop table if exists team;
 drop table if exists member;
 drop table if exists record;
 drop table if exists score;
+
+create table game
+(
+    id   int primary key auto_increment,
+    home_team_id int,
+    away_team_id int
+);
 
 create table team
 (
@@ -14,24 +22,23 @@ create table member
     id       int primary key auto_increment,
     name     varchar(45),
     position varchar(45),
-    team   int references team (id)
+    team     int references team (id)
 );
 
 create table record
 (
-    atBat    int,
+    at_bat    int,
     hit      int,
     `out`    int,
-    average  int,
-    memberId int references member (id),
-    teamId   int references team (id)
+    average  double,
+    member int references member (id)
 );
 
 create table score
 (
-    inningNumber int,
+    inning_number int,
     score        int,
-    teamId       int references team (id)
+    team       int references team (id)
 );
 
 
