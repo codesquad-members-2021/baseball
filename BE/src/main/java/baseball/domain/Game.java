687c7caf0fb27e84ca1dec2334f5615e625b9288
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-06 17:15:32 +0900

    Feat: add Game domain class for game table

---
+++ BE/src/main/java/baseball/domain/Game.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class Game {
+
+    @Id
+    private Long id;
+
+    private Long homeTeamId;
+    private Long awayTeamId;
+
+    public Game(Long homeTeamId, Long awayTeamId) {
+        this.homeTeamId = homeTeamId;
+        this.awayTeamId = awayTeamId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getHomeTeamId() {
+        return homeTeamId;
+    }
+
+    public Long getAwayTeamId() {
+        return awayTeamId;
+    }
+}
