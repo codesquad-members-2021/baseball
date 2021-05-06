caa0d61b8e7ec5ae231a8ebebca72587aa2834d7
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-07 00:52:40 +0900

    Feat: add TeamService to move business logic from controller

---
+++ BE/src/main/java/baseball/service/TeamService.java
@@ -0,0 +1,65 @@
+package baseball.service;
+
+import baseball.domain.Member;
+import baseball.domain.Record;
+import baseball.domain.RecordMember;
+import baseball.domain.Team;
+import baseball.repository.TeamRepository;
+import baseball.service.dto.RequestTeamDTO;
+import baseball.service.dto.TeamDTO;
+import baseball.service.dto.TeamRecordsDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class TeamService {
+
+    private TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public void saveTeams(List<RequestTeamDTO> teamDTOs) {
+        List<Team> teams = teamDTOs.stream()
+                .map(RequestTeamDTO::toTeam)
+                .collect(Collectors.toList());
+        teamRepository.saveAll(teams);
+    }
+
+    public List<TeamDTO> convertToTeamDTOList() {
+        Iterable<Team> teams = teamRepository.findAll();
+        List<TeamDTO> teamDTOs = new ArrayList<>();
+
+        for (Team team : teams) {
+            Set<Member> members = team.getMembers();
+            TeamDTO teamDTO = new TeamDTO(team, members);
+            teamDTOs.add(teamDTO);
+        }
+        return teamDTOs;
+    }
+
+    public void insertRecord(Long teamId, Long memberId, int atBat, int hit, int out) {
+        Team team = teamRepository.findById(teamId).orElseThrow(NoSuchElementException::new);
+        Member member = team.getMemberById(memberId);
+        member.setRecord(new Record(atBat, hit, out));
+        teamRepository.save(team);
+    }
+
+    public TeamRecordsDTO getRecordsOfTeam(Long teamId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(NoSuchElementException::new);
+        Set<Member> members = team.getMembers();
+
+        Set<RecordMember> recordMembers = new HashSet<>();
+        for (Member member : members) {
+            if(member.hasRecord()) {
+                RecordMember recordMember = teamRepository.findRecordByMemberId(member.getId()).get();
+                recordMembers.add(recordMember);
+            }
+        }
+
+        return new TeamRecordsDTO(teamId, recordMembers);
+    }
+}
