2c5d4329d55020681bef2ebdb4f8e0e935f5d99b
Jisoo Kim <ghis22130@naver.com>
2021-05-06 16:26:33 +0900

    feat: [#3] PitchHistoryTableView 생성

---
+++ Baseball/Baseball/Presentation/Common/PaddingLabel.swift
@@ -0,0 +1,26 @@
+//
+//  PaddingLabel.swift
+//  Baseball
+//
+//  Created by 지북 on 2021/05/06.
+//
+
+import UIKit
+
+@IBDesignable class PaddingLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+    let size = super.intrinsicContentSize
+    return CGSize(width: size.width + leftInset + rightInset, height: size.height + topInset + bottomInset)
+    }
+}
---
+++ Baseball/Baseball/Presentation/GamePlayView/View/PitchHistoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  PitchHistoryCell.swift
+//  Baseball
+//
+//  Created by 지북 on 2021/05/06.
+//
+
+import UIKit
+
+class PitchHistoryCell: UITableViewCell {
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var pitchResultLabel: UILabel!
+    @IBOutlet weak var pitchResultTotalLabel: UILabel!
+}
