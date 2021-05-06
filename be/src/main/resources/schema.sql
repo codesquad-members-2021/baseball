1aa9441fe2d017fc3d4a45037bbe5d8c67baaa58
sanhee <68936@naver.com>
2021-05-07 02:32:00 +0900

    [#10] feat: DB 스키마 및 더미 데이터 SQL 작성

---
+++ be/src/main/resources/schema.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS PLAYER_HISTORY;
+DROP TABLE IF EXISTS PLAYER_SCORE;
+DROP TABLE IF EXISTS PLAYER;
+DROP TABLE IF EXISTS TEAM_SCORE;
+DROP TABLE IF EXISTS TEAM;
+DROP TABLE IF EXISTS GAME;
+
+CREATE TABLE GAME
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    round       INT,
+    in_progress TINYINT(1)
+);
+
+CREATE TABLE TEAM
+(
+    id       BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(50),
+    is_home  TINYINT(1),
+    selected TINYINT(1),
+    game_id  BIGINT,
+    FOREIGN KEY (game_id) REFERENCES GAME (id)
+);
+
+CREATE TABLE TEAM_SCORE
+(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    team_id BIGINT,
+    round   INT,
+    score   INT,
+    FOREIGN KEY (team_id) REFERENCES TEAM (id)
+);
+
+CREATE TABLE PLAYER
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50),
+    is_pitcher TINYINT(1),
+    team_id    BIGINT,
+    FOREIGN KEY (team_id) REFERENCES TEAM (id)
+);
+
+CREATE TABLE PLAYER_SCORE
+(
+    player_id        BIGINT PRIMARY KEY,
+    plate_appearance INT,
+    hits             INT,
+    outs             INT,
+    FOREIGN KEY (player_id) REFERENCES PLAYER (id)
+);
+
+CREATE TABLE PLAYER_HISTORY
+(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    player_id BIGINT,
+    round   INT,
+    record   VARCHAR(45),
+    FOREIGN KEY (player_id) REFERENCES PLAYER (id)
+);
