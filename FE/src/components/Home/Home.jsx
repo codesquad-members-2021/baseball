3563ddba0e8c99b072888c6fced779d337ec9dc7
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-06 14:47:51 +0900

    feat: OAuth 구글, 깃허브 로그인 버튼 추가

--- FE/src/components/Home/Home.jsx
+++ FE/src/components/Home/Home.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { GoogleLogin } from 'react-google-login';
+import { githubProvider, googleProvider } from '../../config/authMethods';
+import socialMediaAuth from '../../service/auth';
 
+const handleOnClick = async (provider) => {
+  const res = await socialMediaAuth(provider);
+  console.log(res);
+};
 const Home = () => {
-  const [isLogin, setIsLogin] = useState(false);
-
-  const handleLoginClick = () => setIsLogin(true);
   return (
     <StyledHome>
       this is home.
       <button>
-        <Link to='/intro'>팀 선택하기</Link>
+        <Link to="/intro">팀 선택하기</Link>
       </button>
-      <GoogleLogin
-        clientId='869491322305-t4abjiphkaprn12snvpsqqi1bf8bb3be.apps.googleusercontent.com'
-        buttonText='Login with google'
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
+      <button onClick={() => handleOnClick(githubProvider)}>github</button>
+      <button onClick={() => handleOnClick(googleProvider)}>google</button>
     </StyledHome>
   );
 };
 
 export default Home;
-
-const responseGoogle = (response) => {
-  console.log(response);
-};
 
 const StyledHome = styled.div``;
