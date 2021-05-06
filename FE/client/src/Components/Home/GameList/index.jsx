508866a1ce0f037ce5b94a1c8bab57da0ce7c02d
Jungwon Lee <leejungwon96@gmail.com>
2021-05-06 16:45:51 +0900

    Fix. scroll

--- FE/client/src/Components/Home/GameList/index.jsx
+++ FE/client/src/Components/Home/GameList/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Game from "Components/Home/GameList/Game";
 
@@ -21,10 +21,26 @@ const GameBoxList = styled.ul`
   max-height: 18rem;
   gap: 1rem;
   width: 33rem;
-  overflow-y: scroll;
-  -ms-overflow-style: none;
+  overflow-y: overlay;
   ::-webkit-scrollbar {
+    width: 10px;
     display: none;
+  }
+  &:hover {
+    -ms-overflow-style: none;
+    ::-webkit-scrollbar {
+      width: 10px;
+      display: block;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: #2f3542;
+      border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: grey;
+      border-radius: 10px;
+      box-shadow: inset 0px 0px 5px white;
+    }
   }
 `;
 
