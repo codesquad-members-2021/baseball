98cfba7cb8ba30577659615775caba7efdfae0b0
DongHunKim <ultraraptor@naver.com>
2021-05-07 00:18:46 +0900

    데이터베이스 추가

--- BE_baseball/src/main/resources/data.sql
+++ BE_baseball/src/main/resources/data.sql
@@ -10,4 +10,22 @@ INSERT INTO `match`(home,away) values
     ('Eagles','Giants'),
     ('Giants','Tigers');
 
+-- -- 둘중하나 TOP/AWAY or BOT/HOME
 
+INSERT INTO 'board'(`inning`,`turn`,`strike`,`ball`,`out`,`home_point`,`away_point`,`pitcher`,`batter`) values
+(1,'TOP/AWAY',0,0,0,0,0,'kim','dong'),
+(1,'TOP/AWAY',1,0,0,0,0,'kim','dong'), -- 1 스트라이크
+(1,'TOP/AWAY',2,0,0,0,0,'kim','dong'), -- 2 스트라이크
+(1,'TOP/AWAY',3,0,1,0,0,'kim','dong'), -- 3 스트라이크 , 삼진아웃
+(1,'TOP/AWAY',0,0,1,0,0,'kim','hun'), -- 타자변경
+(1,'TOP/AWAY',0,1,1,0,0,'kim','hun'), -- 1 볼
+(1,'TOP/AWAY',0,2,1,0,0,'kim','hun'), -- 2 볼
+(1,'TOP/AWAY',1,2,1,0,0,'kim','hun'), -- 2 볼 1스트라이크
+(1,'TOP/AWAY',0,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,'TOP/AWAY',1,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,'TOP/AWAY',2,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,'TOP/AWAY',3,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+
+
+
+(1,'BOT/HOME',0,0,0,0,0,'kim','dong');
--- BE_baseball/src/main/resources/schema.sql
+++ BE_baseball/src/main/resources/schema.sql
@@ -1,12 +1,13 @@
 -- schema.sql 파일은 DDL 스크립트
 -- data.sql 파일은 DML 스크립트
+CREATE DATABASE IF NOT EXISTS `baseball`;
 
 use baseball;
 
 drop table IF EXISTS `match`;
 
 CREATE TABLE IF NOT EXISTS `baseball`.`match`(
-`match_id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+`id` BIGINT PRIMARY KEY AUTO_INCREMENT,
 `home` VARCHAR(45) NOT NULL,
 `away` VARCHAR(45) NOT NULL
 );
@@ -18,14 +19,20 @@ DESC `match`;
 
 drop table IF EXISTS `board`;
 
+
+
 CREATE TABLE IF NOT EXISTS `baseball`.`board`
 (
-`game_id` BIGINT PRIMARY KEY AUTO_INCREMENT,
-`turn`    VARCHAR(45) NOT NULL,
-`strike`  INT NOT NULL, -- 점
-`ball`    INT NOT NULL,
-`out`     INT NOT NULL,
-`point`   INT NOT NULL, -- 득
+    `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+`match` BIGINT REFERENCES `match`(id),
+`match_id` INT,
+`inning`  INT NOT NULL, -- 이닝, 몇회차인지
+`turn`    VARCHAR(45) NOT NULL, -- 둘중하나 TOP/AWAY or BOT/HOME
+`strike`  INT NOT NULL, -- 스트라이크 갯수
+`ball`    INT NOT NULL, -- 볼 갯수
+`out`     INT NOT NULL, -- 현 회차에서 아웃한 타자의 수
+`home_point`   INT NOT NULL, -- 홈팀의 득점
+`away_point`   INT NOT NULL, -- 원정팀의 득점
 `pitcher` VARCHAR(45) NOT NULL,
 `batter`  VARCHAR(45) NOT NULL
 );
