7c8c5956124cee11294cd98e6ba4d99af279f95c
jjunyjjuny <jjuny546@gmail.com>
2021-05-06 12:22:30 +0900

    feat : add folder structure, styled components

---
+++ front/front-baseball/src/components/startPage/StartPage.js
@@ -0,0 +1,9 @@
+import styled, { css } from "styled-components";
+const StartPage = () => {
+  return;
+};
+
+const StartPageWrapper = styled.div``
+const StartPageWrapper = styled.div``
+
+export default StartPage;
