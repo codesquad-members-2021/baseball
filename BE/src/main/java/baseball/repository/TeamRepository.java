ad8db5d19db0ecc1a6d2aadd307c8a450c63c5dc
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-06 17:11:12 +0900

    Feat: add joining record table with member table function

--- BE/src/main/java/baseball/repository/TeamRepository.java
+++ BE/src/main/java/baseball/repository/TeamRepository.java
@@ -1,7 +1,15 @@
 package baseball.repository;
 
+import baseball.domain.RecordMember;
 import baseball.domain.Team;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    @Query("select at_bat, hit, `out`, average, name, position from record left join member on record.member = id where id = :id")
+    Optional<RecordMember> findRecordByMemberId(Long id);
+
 }
