bd69ac8fd3b1a415d2187a67e15b53040e4f9358
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-05-06 11:01:56 +0900

    feat: [#3] 게임 플레이 뷰 내부에 그라운드 뷰 추가

---
+++ Baseball/Baseball/Presentation/GamePlayView/View/GamePlayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GamePlayViewController.swift
+//  Baseball
+//
+//  Created by Jun Ho JANG on 2021/05/06.
+//
+
+import UIKit
+
+class GamePlayViewController: UIViewController {
+
+    @IBOutlet weak var groundView: GroundView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
