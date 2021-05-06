7b20350f3892468b1301c85bd006b6459ba9626f
gisungchoi <chlrltjd159@naver.com>
2021-05-06 18:05:06 +0900

    feat : ScoreBoardDAO 정의
    - scoreBoard는 find시 이닝들도 가지고 온다

---
+++ BE/src/main/java/com/codesquad/coco/game/domain/ScoreBoardDAO.java
@@ -0,0 +1,60 @@
+package com.codesquad.coco.game.domain;
+
+import com.codesquad.coco.game.domain.model.Innings;
+import com.codesquad.coco.game.domain.model.ScoreBoard;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Component
+public class ScoreBoardDAO {
+
+    private JdbcTemplate template;
+
+    private InningsDAO inningsDAO;
+
+    public ScoreBoardDAO(DataSource dataSource, InningsDAO inningsDAO) {
+        this.template = new JdbcTemplate(dataSource);
+        this.inningsDAO = inningsDAO;
+    }
+
+    public Long save(ScoreBoard board) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "insert into score_board (game,team) values (?, ?)";
+
+        template.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, board.getGameId());
+            ps.setString(2, board.teamName());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public ScoreBoard findByGameIdAndTeamName(Long id, String teamName) {
+        String sql = "select s.id, s.game,s.team from score_board s where s.game = " + id + " and s.team = '" + teamName + "'";
+
+
+        List<ScoreBoard> query = template.query(sql, (rs, rowNum) -> {
+
+            List<Innings> innings = inningsDAO.findAllById(rs.getLong("id"));
+
+            ScoreBoard board = new ScoreBoard(
+                    rs.getLong("id"),
+                    rs.getLong("game"),
+                    rs.getString("team"),
+                    innings
+            );
+
+            return board;
+        });
+        return query.get(0);
+    }
+}
