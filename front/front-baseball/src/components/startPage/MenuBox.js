c93e3a729c2589dbc39364e84b36f719ca5124bd
jjunyjjuny <jjuny546@gmail.com>
2021-05-07 00:27:52 +0900

    feat : lazy loading fetch, animation

--- front/front-baseball/src/components/startPage/MenuBox.js
+++ front/front-baseball/src/components/startPage/MenuBox.js
@@ -1,25 +1,71 @@
-import { useEffect, useState, useRef } from "react";
-import styled, { css } from "styled-components";
+import { useState, useRef, useCallback, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import { MESSAGE } from "../../utils/constant.js";
 import Game from "./Game";
+import { useInfiniteScroll } from "../../utils/hooks.js";
+
 const MenuBox = () => {
-  const [games, setGames] = useState([1, 2, 3]);
-  const scrollHeight = useRef(0);
-  useEffect(() => {
-    const fetchGamesDate = async () => {
-      const newGames = await dummyFetch();
-      setGames((games) => [...games, ...newGames]);
-    };
-    if (games.length < 10) fetchGamesDate();
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const rootRef = useRef();
+  const targetRef = useRef();
+
+  const requestNewGames = useCallback(async () => {
+    try {
+      setLoading(true);
+      const games = await dummyFetch();
+      return games;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const initLoad = useCallback(async () => {
+    const newGames = await requestNewGames();
+    setGames(newGames);
+  }, [requestNewGames]);
+
+  const loadMoreImage = useCallback(async () => {
+    const newGames = await requestNewGames();
+    setGames((games) => [...games, ...newGames]);
+  }, [requestNewGames]);
+
+  useInfiniteScroll({
+    root: rootRef.current,
+    target: targetRef.current,
+    onIntersect: ([{ isIntersecting }]) => {
+      if (isIntersecting && !loading) {
+        loadMoreImage();
+      }
+    },
   });
+
+  useEffect(() => {
+    if (games.length < 1) {
+      initLoad();
+    }
+  });
+
   return (
     <MenuWrapper>
       <MessageBox>{MESSAGE.INIT}</MessageBox>
-      <Games>
-        {games.map((el) => (
-          <Game />
-        ))}
-      </Games>
+      <GamesContainer>
+        <Games ref={rootRef}>
+          {games.map((el) => (
+            <Game />
+          ))}
+
+          <div ref={targetRef}>{error && <ErrorView />}</div>
+        </Games>
+        {loading && (
+          <LoadingView>
+            <LoadingCircle />
+          </LoadingView>
+        )}
+      </GamesContainer>
     </MenuWrapper>
   );
 };
@@ -72,9 +118,40 @@ const dummyFetch = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(data.games);
-    }, 3000);
+    }, 2000);
   });
 };
+const LoadingView = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.4);
+  font-size: 3rem;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 300px;
+`;
+
+const bounce = keyframes`
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+`;
+
+const LoadingCircle = styled.div`
+  background-color: transparent;
+  border: 5px solid transparent;
+  border-top: 5px solid rgba(189, 215, 60);
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
+  animation: ${bounce} 1s infinite linear;
+`;
+const ErrorView = styled.div``;
 const MenuWrapper = styled.div`
   margin-top: 1rem;
   width: 30%;
@@ -85,8 +162,11 @@ const MessageBox = styled.div`
   margin: 3rem 0;
   font-size: 2rem;
 `;
+const GamesContainer = styled.div`
+  position: relative;
+`;
 const Games = styled.div`
-  height: 500px;
+  height: 300px;
   display: flex;
   flex-direction: column;
   overflow: scroll;
