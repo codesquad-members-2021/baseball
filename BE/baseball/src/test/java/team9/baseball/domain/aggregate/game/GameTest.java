d650ac2b2328efd17feb66f78cbac8bf2609b1ee
정이삭 <isaac56@naver.com>
2021-05-06 17:42:23 +0900

    [#4] test: Create GameTest and Add strike test method
    1. Game 객체의 테스트 클래스 생성
    2. 스트라이크에 따른 상황이 잘 반영되는지 테스트하는 코드 작성

---
+++ BE/baseball/src/test/java/team9/baseball/domain/aggregate/game/GameTest.java
@@ -0,0 +1,83 @@
+package team9.baseball.domain.aggregate.game;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import team9.baseball.domain.aggregate.team.Player;
+import team9.baseball.domain.aggregate.team.Team;
+import team9.baseball.domain.enums.Halves;
+
+class GameTest {
+    private Team homeTeam;
+    private Team awayTeam;
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        homeTeam = new Team("코드스쿼드");
+        homeTeam.setId(1);
+        homeTeam.addPlayer(new Player("아이작", 1));
+        homeTeam.addPlayer(new Player("쏭", 2));
+        homeTeam.addPlayer(new Player("쑤", 3));
+
+        awayTeam = new Team("마스터즈");
+        awayTeam.setId(2);
+        awayTeam.addPlayer(new Player("호눅스", 1));
+        awayTeam.addPlayer(new Player("JK", 2));
+        awayTeam.addPlayer(new Player("크롱", 3));
+
+        game = new Game(awayTeam, homeTeam);
+    }
+
+    @Test
+    @DisplayName("스트라이크 테스트")
+    void strike() {
+        assertCountStatus(0, 0, 0, 0, 0);
+        assertPlayerStatus(1, 1, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(1, 0, 0, 0, 0);
+        assertPlayerStatus(1, 1, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(2, 0, 0, 0, 0);
+        assertPlayerStatus(1, 1, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(0, 0, 1, 0, 0);
+        assertPlayerStatus(1, 2, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(1, 0, 1, 0, 0);
+        assertPlayerStatus(1, 2, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(2, 0, 1, 0, 0);
+        assertPlayerStatus(1, 2, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(0, 0, 2, 0, 0);
+        assertPlayerStatus(1, 3, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(1, 0, 2, 0, 0);
+        assertPlayerStatus(1, 3, null, null, null);
+        game.proceedStrike(awayTeam, homeTeam);
+        assertCountStatus(2, 0, 2, 0, 0);
+        assertPlayerStatus(1, 3, null, null, null);
+    }
+
+    void assertCountStatus(int strike, int ball, int out, int awayScore, int homeScore) {
+        org.junit.jupiter.api.Assertions.assertAll(
+                () -> Assertions.assertThat(game.getStrikeCount()).isEqualTo(strike),
+                () -> Assertions.assertThat(game.getBallCount()).isEqualTo(ball),
+                () -> Assertions.assertThat(game.getOutCount()).isEqualTo(out),
+                () -> Assertions.assertThat(game.getTotalScore(Halves.TOP)).isEqualTo(awayScore),
+                () -> Assertions.assertThat(game.getTotalScore(Halves.BOTTOM)).isEqualTo(homeScore)
+        );
+    }
+
+    void assertPlayerStatus(int pitcherNumber, int batterNumber, Integer base1UniformNumber, Integer base2UniformNumber, Integer base3UniformNumber) {
+        org.junit.jupiter.api.Assertions.assertAll(
+                () -> Assertions.assertThat(game.getPitcherUniformNumber()).isEqualTo(pitcherNumber),
+                () -> Assertions.assertThat(game.getBatterUniformNumber()).isEqualTo(batterNumber),
+                () -> Assertions.assertThat(game.getBase1UniformNumber()).isEqualTo(base1UniformNumber),
+                () -> Assertions.assertThat(game.getBase2UniformNumber()).isEqualTo(base2UniformNumber),
+                () -> Assertions.assertThat(game.getBase3UniformNumber()).isEqualTo(base3UniformNumber)
+        );
+    }
+}
