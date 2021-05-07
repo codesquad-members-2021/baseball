2ac83f943e7e2add892544c84d272cee42a90322
dong <ultraraptor@naver.com>
2021-05-07 13:13:20 +0900

    feat : 설계하다 미래에 변경사항이나 궁금증에 대응하기 위해 주석을 적어놓음(추후 확장을 위해서)

--- BE_baseball/src/main/java/com/dong/baseball/Domain/Player.java
+++ BE_baseball/src/main/java/com/dong/baseball/Domain/Player.java
@@ -1,6 +1,8 @@
 package com.dong.baseball.Domain;
 
 public class Player {
+    // 타자 투수 구별은 없음, 왜냐하면 불펜으로 타자가 투수할수도 있기 때문
+    // 나중에 Player를 최대한 풍부하게 만들어놓고 batter, pitcher 클래스를 상속받아 만들면 됨
     String team;
     int gamesPlayed;
     int atBat;
