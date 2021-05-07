08bc9666c97545e6f41e944323d4d040ed7f3e15
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-07 16:20:39 +0900

    Refactor: replace ScoreDTOs with TeamScoreDTO

--- BE/src/main/java/baseball/service/dto/TeamScoreDTO.java
+++ BE/src/main/java/baseball/service/dto/TeamScoreDTO.java
@@ -4,22 +4,24 @@ import baseball.domain.Score;
 
 import java.util.Set;
 
-public class ScoresDTO {
+public class TeamScoreDTO {
 
     private Long teamId;
+    private String teamName;
     private Set<Score> scores;
 
-    public ScoresDTO(Long teamId, Set<Score> scores) {
+    public TeamScoreDTO(Long teamId, String teamName, Set<Score> scores) {
         this.teamId = teamId;
+        this.teamName = teamName;
         this.scores = scores;
-    }
-
-    public static ScoresDTO toScoreDTO(Long teamId, Set<Score> scores) {
-        return new ScoresDTO(teamId, scores);
     }
 
     public Long getTeamId() {
         return teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
     }
 
     public Set<Score> getScores() {
