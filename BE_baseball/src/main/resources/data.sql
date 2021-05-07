a8914377d7dfeba9a10da3cd5d85c8fa1332f2d0
dong <ultraraptor@naver.com>
2021-05-07 13:11:16 +0900

    feat : board 관련 데이터가 의미있도록 수정

--- BE_baseball/src/main/resources/data.sql
+++ BE_baseball/src/main/resources/data.sql
@@ -1,6 +1,11 @@
 -- schema.sql 파일은 DDL 스크립트
 -- data.sql 파일은 DML 스크립트
 use baseball;
+
+
+-- ----------------------------------------------------------
+-- match 관련 데이터
+DESC `match`;
 INSERT INTO `match`(home,away) values
     ('Tigers','Bears'),
     ('Bears','Twins'),
@@ -9,22 +14,32 @@ INSERT INTO `match`(home,away) values
     ('Lions','Eagles'),
     ('Eagles','Giants'),
     ('Giants','Tigers');
+-- ----------------------------------------------------------
 
--- -- 둘중하나 TOP/AWAY or BOT/HOME
 
-INSERT INTO 'board'(`inning`,`turn`,`strike`,`ball`,`out`,`home_point`,`away_point`,`pitcher`,`batter`) values
-(1,'TOP/AWAY',0,0,0,0,0,'kim','dong'),
-(1,'TOP/AWAY',1,0,0,0,0,'kim','dong'), -- 1 스트라이크
-(1,'TOP/AWAY',2,0,0,0,0,'kim','dong'), -- 2 스트라이크
-(1,'TOP/AWAY',3,0,1,0,0,'kim','dong'), -- 3 스트라이크 , 삼진아웃
-(1,'TOP/AWAY',0,0,1,0,0,'kim','hun'), -- 타자변경
-(1,'TOP/AWAY',0,1,1,0,0,'kim','hun'), -- 1 볼
-(1,'TOP/AWAY',0,2,1,0,0,'kim','hun'), -- 2 볼
-(1,'TOP/AWAY',1,2,1,0,0,'kim','hun'), -- 2 볼 1스트라이크
-(1,'TOP/AWAY',0,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
-(1,'TOP/AWAY',1,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
-(1,'TOP/AWAY',2,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
-(1,'TOP/AWAY',3,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+
+-- ----------------------------------------------------------
+-- board 관련 데이터
+-- -- turn 은 둘중하나 TOP/AWAY or BOT/HOME
+desc `board`;
+INSERT INTO `board`(`match`,`inning`,`turn`,`strike`,`ball`,`out`,`home_point`,`away_point`,`pitcher`,`batter`) values
+(1,1,'TOP/AWAY',0,0,0,0,0,'kim','dong'),
+(1,1,'TOP/AWAY',1,0,0,0,0,'kim','dong'), -- 1 스트라이크
+(1,1,'TOP/AWAY',2,0,0,0,0,'kim','dong'), -- 2 스트라이크
+(1,1,'TOP/AWAY',3,0,1,0,0,'kim','dong'), -- 3 스트라이크 , 삼진아웃
+(1,1,'TOP/AWAY',0,0,1,0,0,'kim','hun'), -- 타자변경
+(1,1,'TOP/AWAY',0,1,1,0,0,'kim','hun'), -- 1 볼
+(1,1,'TOP/AWAY',0,2,1,0,0,'kim','hun'), -- 2 볼
+(1,1,'TOP/AWAY',1,2,1,0,0,'kim','hun'), -- 2 볼 1스트라이크
+(1,1,'TOP/AWAY',0,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,1,'TOP/AWAY',1,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,1,'TOP/AWAY',2,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+(1,1,'TOP/AWAY',3,0,1,0,0,'kim','honux'), -- 앞에서 안타 쳐서 호눅스로 타자변경
+
+
+(1,1,'BOT/HOME',0,0,0,0,0,'kim','dong'); -- */
+select * from board;
+-- ----------------------------------------------------------
 
 
 
