1091c96b14d0de7669423e883b311689c0d051f7
정이삭 <isaac56@naver.com>
2021-05-07 10:30:00 +0900

    [#1] chore: Add `strike_count`, `ball_count` into `pitch_history` table
    pitch_history 테이블에 투구 당시 스트라이크, 볼 상황을 기록하도록 두 개 칼럼 추가하였습니다.

--- BE/baseball/src/main/resources/data.sql
+++ BE/baseball/src/main/resources/data.sql
@@ -1,9 +1,15 @@
-INSERT INTO team (name) VALUES ('코드스쿼드2');
+INSERT INTO team (name) VALUES ('코드쓰쿼드');
 INSERT INTO player (team_id, name, uniform_number) VALUES (1, '아이작', 1);
 INSERT INTO player (team_id, name, uniform_number) VALUES (1, '쏭', 2);
 INSERT INTO player (team_id, name, uniform_number) VALUES (1, '쑤', 3);
+INSERT INTO player (team_id, name, uniform_number) VALUES (1, '쿠퍼', 4);
+INSERT INTO player (team_id, name, uniform_number) VALUES (1, '우디', 5);
 
-INSERT INTO team (name) VALUES ('마스터즈2');
+INSERT INTO team (name) VALUES ('마스터');
 INSERT INTO player (team_id, name, uniform_number) VALUES (2, '호눅스', 1);
 INSERT INTO player (team_id, name, uniform_number) VALUES (2, 'JK', 2);
 INSERT INTO player (team_id, name, uniform_number) VALUES (2, '크롱', 3);
+INSERT INTO player (team_id, name, uniform_number) VALUES (2, '세라', 4);
+INSERT INTO player (team_id, name, uniform_number) VALUES (2, '헤드', 5);
+
+INSERT INTO `user` (email) VALUES ('test@test.com');
