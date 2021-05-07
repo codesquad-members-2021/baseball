b2cd5a46f9c2ef1681bb252186d8ff1447f5769f
Raccoon <nobllizdc@gmail.com>
2021-05-07 15:12:19 +0900

    fix: Fix squadboard popup (#19)

--- FE/baseball/src/Components/Game/SquadBoard/SquadBoard.jsx
+++ FE/baseball/src/Components/Game/SquadBoard/SquadBoard.jsx
@@ -1,30 +1,25 @@
-import { useState } from "react";
-import PopUpButton from "./PopUpButton";
-import SquadTable from "./SquadTable/SquadTable";
-import { SquadBoard as S } from "@/Components/Game/GameStyles";
+import { useState } from 'react';
+import PopUpButton from './PopUpButton';
+import SquadTable from './SquadTable/SquadTable';
+import { SquadBoard as S } from '@/Components/Game/GameStyles';
 
 const SquadBoard = () => {
   const [mouseOverFlag, setMouseOverFlag] = useState(false);
 
   const handleMouseOver = ({ target: { tagName } }) => {
-    if (tagName === "path" || tagName === "svg") return;
+    if (tagName === 'path' || tagName === 'svg') return;
     setMouseOverFlag((prev) => !prev);
   };
 
   return (
-    <>
-      <S.SquadBoardWrapper>
-        <S.PopUpBackground
-          isMouseOver={mouseOverFlag}
-          onMouseOver={handleMouseOver}
-        />
-        <PopUpButton handleMouseOver={handleMouseOver} />
-        <S.SquadBoard isMouseOver={mouseOverFlag}>
-          <SquadTable />
-          <SquadTable />
-        </S.SquadBoard>
-      </S.SquadBoardWrapper>
-    </>
+    <S.SquadBoardWrapper>
+      <S.PopUpBackground isMouseOver={mouseOverFlag} />
+      <PopUpButton handleMouseOver={handleMouseOver} />
+      <S.SquadBoard isMouseOver={mouseOverFlag} onMouseLeave={handleMouseOver}>
+        <SquadTable />
+        <SquadTable />
+      </S.SquadBoard>
+    </S.SquadBoardWrapper>
   );
 };
 
