dc52e0b45466b97e1a56a172146742b9652d31b0
Polynomeer <polynomeer@gmail.com>
2021-05-07 13:42:32 +0900

    chore: Update schema.sql and data.sql (#7)
    Updated SQL scripts by changes of DB schema.

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,6 +1,27 @@
-INSERT INTO `baseball`.`game` (`game_id`, `home`, `away`, `home_score`) VALUES ('1', 'Captain', 'Marvel', '1');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('1', 'Samsung Lions', '0');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('2', 'KIA Tigers', '1');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('3', 'NC Dinos', '0');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('4', 'Hanwha Eagles', '0');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('5', 'Lotte Giants', '1');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('6', 'Doosan Bears', '0');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('7', 'SSG Landers', '0');
+INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('8', 'LG Twins', '0');
 
-INSERT INTO `baseball`.`inning` (`inning_id`, `offense_team`, `inning`, `score`, `ball`, `strike`, `out`, `hitter`, `first_base`, `second_base`, `game_id`) VALUES ('1', 'Captain', '1', '1', '2', '2', '1', '6', '5', '4', '1');
-
-INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('1', 'Captain', '1');
-INSERT INTO `baseball`.`team` (`team_id`, `name`, `isPlaying`) VALUES ('2', 'Marvel', '0');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('1', '1', '강민호', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('2', '2', '강한울', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('3', '3', '구자욱', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('4', '4', '김상수', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('5', '5', '김성윤', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('6', '6', '김현곤', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('7', '7', '박해민', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('8', '8', '라이블리', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('9', '9', '오승환', '0', '0', '0', '0.0', '1');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('10', '1', '고영창', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('11', '2', '권혁경', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('12', '3', '김규성', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('13', '4', '김원경', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('14', '5', '나주환', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('15', '6', '박건우', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('16', '7', '박찬호', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('17', '8', '브룩스', '0', '0', '0', '0.0', '2');
+INSERT INTO `baseball`.`player` (`player_id`, `number`, `name`, `pa`, `hit`, `out`, `average`, `team_id`) VALUES ('18', '9', '양승철', '0', '0', '0', '0.0', '2');
--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `player`;
 DROP TABLE IF EXISTS `team`;
 DROP TABLE IF EXISTS `inning`;
-DROP TABLE IF EXISTS `playing_log`;
+DROP TABLE IF EXISTS `playing`;
 DROP TABLE IF EXISTS `game`;
 
 -- MySQL Workbench Forward Engineering
@@ -24,6 +24,8 @@ USE `baseball`;
 -- -----------------------------------------------------
 -- Table `baseball`.`team`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `baseball`.`team`;
+
 CREATE TABLE IF NOT EXISTS `baseball`.`team`
 (
     `team_id`   INT         NOT NULL,
@@ -38,16 +40,17 @@ CREATE TABLE IF NOT EXISTS `baseball`.`team`
 -- -----------------------------------------------------
 -- Table `baseball`.`player`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `baseball`.`player`;
+
 CREATE TABLE IF NOT EXISTS `baseball`.`player`
 (
     `player_id` INT         NOT NULL,
+    `number`    INT         NOT NULL,
     `name`      VARCHAR(45) NOT NULL,
-    `position`  VARCHAR(45) NOT NULL,
-    `pitch`     INT         NULL,
     `pa`        INT         NULL,
     `hit`       INT         NULL,
     `out`       INT         NULL,
-    `average`   INT         NULL,
+    `average`   DOUBLE      NULL,
     `team_id`   INT         NOT NULL,
     PRIMARY KEY (`player_id`, `team_id`),
     INDEX `fk_player_team_idx` (`team_id` ASC) VISIBLE,
@@ -63,6 +66,8 @@ CREATE TABLE IF NOT EXISTS `baseball`.`player`
 -- -----------------------------------------------------
 -- Table `baseball`.`game`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `baseball`.`game`;
+
 CREATE TABLE IF NOT EXISTS `baseball`.`game`
 (
     `game_id`    INT         NOT NULL AUTO_INCREMENT,
@@ -70,26 +75,32 @@ CREATE TABLE IF NOT EXISTS `baseball`.`game`
     `away`       VARCHAR(45) NOT NULL,
     `home_score` INT         NULL,
     `away_score` INT         NULL,
+    `isEnd`      TINYINT(1)  NOT NULL,
     PRIMARY KEY (`game_id`)
 )
     ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `baseball`.`playing_log`
+-- Table `baseball`.`playing`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `baseball`.`playing_log`
+DROP TABLE IF EXISTS `baseball`.`playing`;
+
+CREATE TABLE IF NOT EXISTS `baseball`.`playing`
 (
-    `log_id`      INT         NOT NULL AUTO_INCREMENT,
-    `player_name` VARCHAR(45) NOT NULL,
-    `pitch`       INT         NULL,
-    `pa`          INT         NULL,
-    `hit`         INT         NULL,
-    `action`      VARCHAR(45) NULL,
-    `total_count` INT         NULL,
-    `game_id`     INT         NOT NULL,
+    `log_id`        INT         NOT NULL AUTO_INCREMENT,
+    `team_id`       VARCHAR(45) NOT NULL,
+    `player_number` INT         NOT NULL,
+    `player_name`   VARCHAR(45) NOT NULL,
+    `pa`            INT         NULL,
+    `hit`           INT         NULL,
+    `out`           INT         NULL,
+    `average`       DOUBLE      NULL,
+    `game_id`       INT         NOT NULL,
     PRIMARY KEY (`log_id`, `game_id`),
     INDEX `fk_playing_log_game1_idx` (`game_id` ASC) VISIBLE,
+    UNIQUE INDEX `player_number_UNIQUE` (`player_number` ASC) VISIBLE,
+    UNIQUE INDEX `team_name_UNIQUE` (`team_id` ASC) VISIBLE,
     CONSTRAINT `fk_playing_log_game1`
         FOREIGN KEY (`game_id`)
             REFERENCES `baseball`.`game` (`game_id`)
@@ -102,23 +113,18 @@ CREATE TABLE IF NOT EXISTS `baseball`.`playing_log`
 -- -----------------------------------------------------
 -- Table `baseball`.`inning`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `baseball`.`inning`;
+
 CREATE TABLE IF NOT EXISTS `baseball`.`inning`
 (
-    `inning_id`    INT         NOT NULL AUTO_INCREMENT,
-    `offense_team` VARCHAR(45) NOT NULL,
-    `inning`       INT         NOT NULL,
-    `score`        INT         NULL,
-    `ball`         INT         NULL,
-    `strike`       INT         NULL,
-    `out`          INT         NULL,
-    `hitter`       INT         NULL,
-    `first_base`   INT         NULL,
-    `second_base`  INT         NULL,
-    `third_base`   INT         NULL,
-    `game_id`      INT         NOT NULL,
-    PRIMARY KEY (`inning_id`, `game_id`),
-    INDEX `fk_inning_game1_idx` (`game_id` ASC) VISIBLE,
-    CONSTRAINT `fk_inning_game1`
+    `inninig_id` INT NOT NULL,
+    `team_id`    INT NOT NULL,
+    `score`      INT NULL,
+    `game_id`    INT NOT NULL,
+    PRIMARY KEY (`inninig_id`, `game_id`),
+    UNIQUE INDEX `team_id_UNIQUE` (`team_id` ASC) VISIBLE,
+    INDEX `fk_inninig_game1_idx` (`game_id` ASC) VISIBLE,
+    CONSTRAINT `fk_inninig_game1`
         FOREIGN KEY (`game_id`)
             REFERENCES `baseball`.`game` (`game_id`)
             ON DELETE NO ACTION
