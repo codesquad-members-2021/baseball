469368462db10dd4bb999a4197b40117962dabbd
Sally Oh <min27604@hotmail.com>
2021-05-07 15:08:03 +0900

    test: 테스트 데이터 작성

---
+++ BE/baseball/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `team`(name, is_home) VALUES ('Captain', 1), ('Marvel', 0), ('Tigers', 1), ('Giants', 0), ('team5', 1), ('team6', 0);
+
+INSERT INTO `game`(home_team_id, away_team_id) VALUES (1, 2), (3, 4), (5, 6);
