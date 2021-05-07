2ce276f15732f41a1752093742f9542790e4ef5d
1song2 <1song2918@gmail.com>
2021-05-07 17:30:13 +0900

    feature: add bases using CALayer and UIBezierPath

--- iOS/BaseballApp/BaseballApp/Views/GroundView.swift
+++ iOS/BaseballApp/BaseballApp/Views/GroundView.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class GroundView: UIView {
-    
     static let identifier = String(describing: GroundView.self)
     let infieldSquareLayer = CAShapeLayer()
     let homePlateLayer = CAShapeLayer()
+    let firstBaseLayer = CAShapeLayer()
+    let secondBaseLayer = CAShapeLayer()
+    let thirdBaseLayer = CAShapeLayer()
     
     override func awakeFromNib() {
         self.backgroundColor = .systemGray3
@@ -27,15 +29,34 @@ class GroundView: UIView {
         
         layer.addSublayer(homePlateLayer)
         homePlateLayer.fillColor = UIColor.white.cgColor
+        
+        layer.addSublayer(firstBaseLayer)
+        firstBaseLayer.strokeColor = UIColor.systemGray.cgColor
+        firstBaseLayer.fillColor = UIColor.white.cgColor
+        firstBaseLayer.lineWidth = 1.0
+        
+        layer.addSublayer(secondBaseLayer)
+        secondBaseLayer.strokeColor = UIColor.systemGray.cgColor
+        secondBaseLayer.fillColor = UIColor.white.cgColor
+        secondBaseLayer.lineWidth = 1.0
+        
+        layer.addSublayer(thirdBaseLayer)
+        thirdBaseLayer.strokeColor = UIColor.systemGray.cgColor
+        thirdBaseLayer.fillColor = UIColor.white.cgColor
+        thirdBaseLayer.lineWidth = 1.0
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
         let squarePath = UIBezierPath()
+        /// Home Plate
         squarePath.move(to: CGPoint(x: bounds.midX, y: bounds.maxY - 50.0))
+        /// First Plate
         squarePath.addLine(to: CGPoint(x: bounds.maxX - 50.0, y: bounds.midY))
+        /// Second Plate
         squarePath.addLine(to: CGPoint(x: bounds.midX, y: bounds.minY + 50.0))
+        /// Third Plate
         squarePath.addLine(to: CGPoint(x: bounds.minX + 50.0, y: bounds.midY))
         squarePath.close()
         infieldSquareLayer.path = squarePath.cgPath
@@ -48,5 +69,29 @@ class GroundView: UIView {
         platePath.addLine(to: CGPoint(x: bounds.midX + 15.0, y: bounds.maxY - 55.0))
         platePath.close()
         homePlateLayer.path = platePath.cgPath
+        
+        let firstBasePath = UIBezierPath()
+        firstBasePath.move(to: CGPoint(x: bounds.maxX - 65.0, y: bounds.midY))
+        firstBasePath.addLine(to: CGPoint(x: bounds.maxX - 50.0, y: bounds.midY - 15.0))
+        firstBasePath.addLine(to: CGPoint(x: bounds.maxX - 35.0, y: bounds.midY))
+        firstBasePath.addLine(to: CGPoint(x: bounds.maxX - 50.0, y: bounds.midY + 15.0))
+        firstBasePath.close()
+        firstBaseLayer.path = firstBasePath.cgPath
+        
+        let secondBasePath = UIBezierPath()
+        secondBasePath.move(to: CGPoint(x: bounds.midX - 15.0, y: bounds.minY + 50.0))
+        secondBasePath.addLine(to: CGPoint(x: bounds.midX, y: bounds.minY + 35.0))
+        secondBasePath.addLine(to: CGPoint(x: bounds.midX + 15.0, y: bounds.minY + 50.0))
+        secondBasePath.addLine(to: CGPoint(x: bounds.midX, y: bounds.minY + 65.0))
+        secondBasePath.close()
+        secondBaseLayer.path = secondBasePath.cgPath
+        
+        let thirdBasePath = UIBezierPath()
+        thirdBasePath.move(to: CGPoint(x: bounds.minX + 35.0, y: bounds.midY))
+        thirdBasePath.addLine(to: CGPoint(x: bounds.minX + 50.0, y: bounds.midY - 15.0))
+        thirdBasePath.addLine(to: CGPoint(x: bounds.minX + 65.0, y: bounds.midY))
+        thirdBasePath.addLine(to: CGPoint(x: bounds.minX + 50.0, y: bounds.midY + 15.0))
+        thirdBasePath.close()
+        thirdBaseLayer.path = thirdBasePath.cgPath
     }
 }
