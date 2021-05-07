3da1b903a8e93a26df2197a89bcc77c0222b4ab2
jeonyeonkyu <ehb6915@naver.com>
2021-05-07 19:52:18 +0900

    Test: router 테스트

--- FE/client/src/App.js
+++ FE/client/src/App.js
@@ -1,5 +1,8 @@
-import { createGlobalStyle } from "styled-components";
-import Page from "Components/Page";
+import React from "react";
+import {
+  Router, Route, Link
+} from "./utils/Router.jsx";
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export const GlobalStyle = createGlobalStyle`
   *{
@@ -24,13 +27,51 @@ export const GlobalStyle = createGlobalStyle`
   }
 `;
 
-function App() {
-  return (
+return (
+  <Router>
     <div>
-      <GlobalStyle />
-      <Page />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+
     </div>
-  );
+  </Router>
+);
 }
 
-export default App;
+function Home() {
+  console.log('I am Home')
+  return <h2>Home</h2>;
+}
+
+function About() {
+  console.log('I am About')
+  return <h2>About</h2>;
+}
+
+function Users() {
+  console.log('I am Users')
+  return <h2>Users</h2>;
+}
