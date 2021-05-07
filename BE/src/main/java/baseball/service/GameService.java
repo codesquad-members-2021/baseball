19d2101c6a14fe5a7826d658a19ae9a9540087ae
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-07 16:37:50 +0900

    Feature: move business logic to GameService from controller

---
+++ BE/src/main/java/baseball/service/GameService.java
@@ -0,0 +1,77 @@
+package baseball.service;
+
+import baseball.domain.Game;
+import baseball.domain.Score;
+import baseball.domain.Team;
+import baseball.repository.GameRepository;
+import baseball.repository.TeamRepository;
+import baseball.service.dto.GameDTO;
+import baseball.service.dto.GameScoreDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class GameService {
+
+    private static final int NUMBER_OF_TEAM = 10;
+
+    private GameRepository gameRepository;
+    private TeamRepository teamRepository;
+
+    public GameService(GameRepository gameRepository, TeamRepository teamRepository) {
+        this.gameRepository = gameRepository;
+        this.teamRepository = teamRepository;
+    }
+
+    public List<GameDTO> getGameDTOList() {
+        if (gameRepository.count() != 5) {
+            saveGames();
+        }
+
+        List<GameDTO> gameDTOs = new ArrayList<>();
+        Iterable<Game> games = gameRepository.findAll();
+
+        for (Game game : games) {
+            Long id = game.getId();
+            Team homeTeam = teamRepository.findById(game.getHomeTeamId()).orElseThrow(NoSuchElementException::new);
+            Team awayTeam = teamRepository.findById(game.getAwayTeamId()).orElseThrow(NoSuchElementException::new);
+
+            GameDTO gameDTO = new GameDTO(id, homeTeam.getName(), awayTeam.getName());
+            gameDTOs.add(gameDTO);
+        }
+        return gameDTOs;
+    }
+
+    private void saveGames() {
+        List<Game> games = new ArrayList<>();
+
+        for (long i = 1; i <= NUMBER_OF_TEAM; i += 2) {
+            Team homeTeam = teamRepository.findById(i).orElseThrow(NoSuchElementException::new);
+            Team awayTeam = teamRepository.findById(i + 1).orElseThrow(NoSuchElementException::new);
+
+            Game game = new Game(homeTeam.getId(), awayTeam.getId());
+            games.add(game);
+        }
+        gameRepository.saveAll(games);
+    }
+
+    public void saveScore(Long teamId, int inningNumber, int score) {
+        Team team = teamRepository.findById(teamId).orElseThrow(NoSuchElementException::new);
+
+        Score newScore = new Score(inningNumber, score);
+        team.setScore(newScore);
+
+        teamRepository.save(team);
+    }
+
+    public GameScoreDTO convertToGameScoreDTO(Long gameId) {
+        Game game = gameRepository.findById(gameId).orElseThrow(NoSuchElementException::new);
+        Team homeTeam = teamRepository.findById(game.getHomeTeamId()).orElseThrow(NoSuchElementException::new);
+        Team awayTeam = teamRepository.findById(game.getAwayTeamId()).orElseThrow(NoSuchElementException::new);
+
+        return new GameScoreDTO(gameId, homeTeam, awayTeam);
+    }
+}
