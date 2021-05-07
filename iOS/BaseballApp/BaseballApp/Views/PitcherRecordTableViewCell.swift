05030106a725ffe1d29ceaca52cb6147e979deeb
1song2 <1song2918@gmail.com>
2021-05-07 17:31:15 +0900

    refactor: add corner radius to number label

--- iOS/BaseballApp/BaseballApp/Views/PitcherRecordTableViewCell.swift
+++ iOS/BaseballApp/BaseballApp/Views/PitcherRecordTableViewCell.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class PitcherRecordTableViewCell: UITableViewCell {
+    @IBOutlet weak var numberLabel: UILabel!
     
     static let identifier = String(describing: PitcherRecordTableViewCell.self)
 
     static func nib() -> UINib {
         return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        numberLabel.layer.masksToBounds = true
+        numberLabel.layer.cornerRadius = numberLabel.frame.width / 2
     }
 }
