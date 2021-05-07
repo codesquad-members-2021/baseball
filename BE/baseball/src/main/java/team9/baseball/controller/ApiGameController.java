6b039c82e155c7a8546742fb2b34a515ec91a655
정이삭 <isaac56@naver.com>
2021-05-07 10:24:15 +0900

    [#6] feat: Create ApiGameController
    인증과정과 Request param을 받는 걸 생략한 game관련 contoroller입니다. (추후 추가 예정)

---
+++ BE/baseball/src/main/java/team9/baseball/controller/ApiGameController.java
@@ -0,0 +1,42 @@
+package team9.baseball.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import team9.baseball.DTO.response.ApiResult;
+import team9.baseball.domain.enums.PitchResult;
+import team9.baseball.domain.enums.Venue;
+import team9.baseball.service.GameService;
+
+@RestController
+@RequestMapping("/game")
+public class ApiGameController {
+    private final GameService gameService;
+
+    @Autowired
+    public ApiGameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping
+    public ApiResult createGame() {
+        gameService.createNewGame(1l, 1, 2);
+        return ApiResult.succeed("OK");
+    }
+
+    @PostMapping("/joining")
+    public ApiResult joinGame() {
+        gameService.joinGame(1l, 1, Venue.HOME);
+        return ApiResult.succeed("OK");
+    }
+
+    @GetMapping("/status")
+    public ApiResult getCurrentGameStatus() {
+        return ApiResult.succeed(gameService.getCurrentGameStatus(1l));
+    }
+
+    @PutMapping("/status")
+    public ApiResult pitch(PitchResult pitchResult) {
+        gameService.applyPitchResult(1l, pitchResult);
+        return ApiResult.succeed("OK");
+    }
+}
