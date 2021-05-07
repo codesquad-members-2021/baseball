3ab14473ee6c372d51f648a2e8a298d1ba775123
Sally Oh <min27604@hotmail.com>
2021-05-07 15:10:55 +0900

    feat: DTO 포맷에 맞게 테이블 결과를 불러오는 쿼리 작성
    Game에 id, 게임이 가진 두 팀 아이디를 바탕으로 조인을 이용해 팀 이름 가져오기

--- BE/baseball/src/main/java/com/codesquad/baseball/repository/GameRepository.java
+++ BE/baseball/src/main/java/com/codesquad/baseball/repository/GameRepository.java
@@ -1,8 +1,18 @@
 package com.codesquad.baseball.repository;
 
+import com.codesquad.baseball.DTO.GameListDTO;
 import com.codesquad.baseball.domain.Game;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GameRepository extends CrudRepository<Game, Long> {
+
+    @Query("select game.id as gameId,\n" +
+            "\t(Select team.name from game left join team on game.home_team_id = team.id where game.id = gameId) as homeTeamName,\n" +
+            "\t(Select team.name from game left join team on game.away_team_id = team.id where game.id = gameId) as awayTeamName\n" +
+            "From game;")
+    List<GameListDTO> browseAllGames();
 
 }
