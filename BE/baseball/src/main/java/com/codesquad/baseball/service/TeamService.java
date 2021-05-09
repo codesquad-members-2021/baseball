03726711a4e6b35baba72df3e13f9f2afb7aa354
Sally Oh <min27604@hotmail.com>
2021-05-09 21:12:09 +0900

    feat: TeamService 구현
    우선은 아이디로 팀을 찾아주는 메서드만 구현했다

---
+++ BE/baseball/src/main/java/com/codesquad/baseball/service/TeamService.java
@@ -0,0 +1,28 @@
+package com.codesquad.baseball.service;
+
+import com.codesquad.baseball.DTO.PlayerDTO;
+import com.codesquad.baseball.domain.Player;
+import com.codesquad.baseball.domain.Team;
+import com.codesquad.baseball.error.exception.TeamNotFoundException;
+import com.codesquad.baseball.repository.TeamRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public Team save(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public Team browseTeamById(Long id) {
+        return teamRepository.findById(id).orElseThrow(TeamNotFoundException::new);
+    }
+}
