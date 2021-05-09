c9117a6aba52daa5b1b0defa2248d2e0660c933e
Sally Oh <min27604@hotmail.com>
2021-05-10 01:22:49 +0900

    feat: 입장할 수 있는 게임인지 상태를 가져오는 browseGameStatus 구현
    맞춰둔 형식에 맞게 반환하기 위해 GameStatusDTO를 반환한다

--- BE/baseball/src/main/java/com/codesquad/baseball/controller/GameController.java
+++ BE/baseball/src/main/java/com/codesquad/baseball/controller/GameController.java
@@ -1,12 +1,16 @@
 package com.codesquad.baseball.controller;
 
 import com.codesquad.baseball.DTO.GameListDTO;
+import com.codesquad.baseball.DTO.GameStatusDTO;
 import com.codesquad.baseball.DTO.TeamRecordDTO;
 import com.codesquad.baseball.domain.Game;
 import com.codesquad.baseball.domain.Team;
 import com.codesquad.baseball.service.GameService;
 import com.codesquad.baseball.service.TeamService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -25,6 +29,14 @@ public class GameController {
     @GetMapping("/games")
     public List<GameListDTO> browseGames() {
         return gameService.browseAllGames();
+    }
+
+    @GetMapping("/games/{gameId}")
+    public GameStatusDTO browseGameStatus(@PathVariable Long gameId) {
+        Game game = gameService.findGameById(gameId);
+        Team homeTeam = teamService.browseTeamById(game.getHomeTeamId());
+        Team awayTeam = teamService.browseTeamById(game.getAwayTeamId());
+        return GameStatusDTO.of(game, homeTeam, awayTeam);
     }
 
     @GetMapping("/play/{gameId}/players")
