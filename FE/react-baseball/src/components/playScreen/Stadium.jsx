da481e549b7455b73d54b19c9fc4b21893ada26b
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-05-10 03:49:39 +0900

    feat: Stadium 야구장 사진 입력
    - img -> backgroundimg로 변경
    - min27604/baseball/#11

--- FE/react-baseball/src/components/playScreen/Stadium.jsx
+++ FE/react-baseball/src/components/playScreen/Stadium.jsx
@@ -3,14 +3,32 @@ import styled from 'styled-components';
 const Stadium = () => {
   return (
     <StadiumDiv>
-      <div>Stadium</div>
+      {/* <ImgDiv> */}
+      {/* <img src='./baseballRound.jpg' width='100%' height='100%' /> */}
+      {/* <img src={require('./baseballRound.jpg')} /> */}
+      {/* </ImgDiv> */}
     </StadiumDiv>
   );
 };
 
 const StadiumDiv = styled.div`
-  background-image: url('../../utils/baseballRound.jpg');
-  z-index: 10;
+  box-sizing: border-box;
+  opacity: 40%;
+  background-image: url('./baseballRound.jpg');
+  background-position: center;
+  background-size: cover;
+  /* overflow: hidden; */
+  /* position: relative; */
+`;
+const ImgDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: fit-content;
+  height: fit-content;
+  background-color: black;
+  opacity: 0.5;
+  /* border: 4px solid red; */
 `;
 
 export default Stadium;
