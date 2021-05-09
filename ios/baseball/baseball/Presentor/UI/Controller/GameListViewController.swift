06e6a7de1ebc53793123c9110ce593ba8918ac51
zombietux <>
2021-05-09 22:44:57 +0900

    feat:RxSwift를 활용한 데이터 바인딩 (#29)

--- ios/baseball/baseball/Presentor/UI/Controller/GameListViewController.swift
+++ ios/baseball/baseball/Presentor/UI/Controller/GameListViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class GameListViewController: UIViewController {
 
@@ -15,26 +17,38 @@ class GameListViewController: UIViewController {
         Match(home: "Marvel", away: "Captin", id: "1"),
     ]
     
+    private var viewModel = MatchViewModel()
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView.dataSource = self
+//        self.collectionView.dataSource = self
+//        viewModel.fetchMatchs()
+        viewModel.matchs
+            .observe(on: MainScheduler.instance)
+            .bind(to: collectionView.rx.items(cellIdentifier: MatchCell.reuseIdentifier, cellType: MatchCell.self)) { index, item, cell in
+                cell.awayTeamLabel.text = item.away
+                cell.homeTeamLabel.text = item.home
+                cell.numberLabel.text = "GAME \(index+1)"
+            }
+            .disposed(by: disposeBag)
     }
 }
 
-extension GameListViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.gameList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MatchCell.reuseIdentifier, for: indexPath) as? MatchCell else {
-            return UICollectionViewCell()
-        }
-        
-        let match = self.gameList[indexPath.row]
-        cell.configureCell(match)
-        
-        return cell
-    }
-}
+//extension GameListViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.gameList.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MatchCell.reuseIdentifier, for: indexPath) as? MatchCell else {
+//            return UICollectionViewCell()
+//        }
+//        
+//        let match = self.gameList[indexPath.row]
+//        cell.configureCell(match)
+//        
+//        return cell
+//    }
+//}
 
