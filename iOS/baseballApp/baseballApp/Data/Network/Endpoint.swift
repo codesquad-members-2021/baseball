5eb020601d8342e2250455eff4ab4891f98da61f
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-10 17:04:19 +0900

    Fix: mockServer url 수정

--- iOS/baseballApp/baseballApp/Data/Network/Endpoint.swift
+++ iOS/baseballApp/baseballApp/Data/Network/Endpoint.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 struct Endpoint {
-    static let getGame = "https://791e125f-f50f-4801-90d3-3b106e723a88.mock.pstmn.io/baseball"
+    static let getGame = "https://c1f9a61e-4e5f-4652-9e28-d0059f40ce4b.mock.pstmn.io/baseball"
     static let postGame = "https://c1f9a61e-4e5f-4652-9e28-d0059f40ce4b.mock.pstmn.io/game"
 }
