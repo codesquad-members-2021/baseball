af4899c6aed1e4126d76966d0905757748e6cbb9
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-11 06:31:07 +0900

    style: pitch 버튼 만듬

--- fe/src/components/GamePlay/playScreen/Screen.jsx
+++ fe/src/components/GamePlay/playScreen/Screen.jsx
@@ -1,11 +1,15 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import BallCount from 'components/GamePlay/playScreen/BallCount'
+import { IoIosBaseball } from 'react-icons/io'
 import Span from 'components/common/Span'
 const Screen = () => {
   return (
     <StyledScreen>
       <ScreenField src='field.svg' alt='field' />
       <ScreenRound>2회 초 수비</ScreenRound>
+      <PitchButton>
+        <IoIosBaseball />
+      </PitchButton>
       <BallCount />
     </StyledScreen>
   )
@@ -33,4 +37,27 @@ const ScreenRound = styled(Span)`
   position: absolute;
   right: 2%;
   top: 4%;
+`
+const buttonAnimation = keyframes`
+
+100% {
+    	transform: rotate(360deg);
+    }
+`
+
+const PitchButton = styled.button`
+  height: 5rem;
+  position: absolute;
+  left: 47%;
+  top: 43%;
+  cursor: pointer;
+  font-size: 5rem;
+  color: white;
+  background: none;
+  &:hover {
+    /* transition: ease-in 0.5s; */
+    animation: ${buttonAnimation} 5s linear infinite;
+    animation-delay: 0.5s;
+    transform-origin: 50% 50%;
+  }
 `
