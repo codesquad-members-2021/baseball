8c8fd7832d2f2e845d0c33dc823b74917c7e90c6
Ji-Sun Kim <jisunkim98@gmail.com>
2021-05-10 16:51:20 +0900

    Chore: .gitignore 수정

--- iOS/baseballApp/Pods/Target Support Files/Pods-baseballApp/Pods-baseballApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_baseballApp : NSObject
-@end
-@implementation PodsDummy_Pods_baseballApp
-@end
--- iOS/baseballApp/Pods/Target Support Files/Pods-baseballAppTests/Pods-baseballAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_baseballAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_baseballAppTests
-@end
