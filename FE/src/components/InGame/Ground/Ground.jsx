f93ccf158aa93a7950840ee1982a5566455b1a38
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-10 14:10:14 +0900

    fix: conflict3

--- FE/src/components/InGame/Ground/Ground.jsx
+++ FE/src/components/InGame/Ground/Ground.jsx
@@ -1,30 +1,20 @@
-import styled from "styled-components";
-<<<<<<< HEAD
-import Header from "./Header";
-import Main from "./Main"
-=======
-import Score from "./Score";
-import Field from "./Field"
->>>>>>> 1e3645475e93f1fb9d1a80dcb58913934e539724
+import styled from 'styled-components';
+import Score from './Score';
+import Field from './Field';
 
 const Ground = () => {
-	return (
-		<StyledGround>
-<<<<<<< HEAD
-			<Header />
-			<Main />
-=======
-			<Score />
-			<Field />
->>>>>>> 1e3645475e93f1fb9d1a80dcb58913934e539724
-		</StyledGround>
-	);
+  return (
+    <StyledGround>
+      <Score />
+      <Field />
+    </StyledGround>
+  );
 };
 
 const StyledGround = styled.div`
-	width: 960px;
-	height: 720px;
-	border-right: 3px solid gray;
+  width: 960px;
+  height: 720px;
+  border-right: 3px solid gray;
 `;
 
 export default Ground;
