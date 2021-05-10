6ddbfdbab5f0f6aa2f1623ccd16e3be0497667b5
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-10 17:04:39 +0900

    Style: screen component 생성

---
+++ fe/src/components/GamePlay/playScreen/Screen.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Screen = () => {
+  return <StyledScreen></StyledScreen>
+}
+
+export default Screen
+
+const StyledScreen = styled.section`
+  width: 70%;
+  height: 100%;
+`
