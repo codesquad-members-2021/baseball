72c63757a8514c31052496c4582f610a607ae7a9
gisungchoi <chlrltjd159@naver.com>
2021-05-10 16:06:01 +0900

    refactor : service로 책임 이전
    - 게임 DTO를 받는다

--- BE/src/main/java/com/codesquad/coco/game/GameService.java
+++ BE/src/main/java/com/codesquad/coco/game/GameService.java
@@ -2,9 +2,12 @@ package com.codesquad.coco.game;
 
 import com.codesquad.coco.game.domain.DAO.GameDAO;
 import com.codesquad.coco.game.domain.DAO.ScoreBoardDAO;
+import com.codesquad.coco.game.domain.model.DTO.GameScoreDTO;
 import com.codesquad.coco.game.domain.model.Innings;
 import com.codesquad.coco.game.domain.model.ScoreBoard;
 import com.codesquad.coco.team.domain.DTO.TeamPointDTO;
+import com.codesquad.coco.team.domain.DTO.TeamScoreDTO;
+import com.codesquad.coco.utils.DTOConverter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,6 +32,19 @@ public class GameService {
         boardDAO.saveInnings(innings);
     }
 
+    public GameScoreDTO findGameScoreDTOByGameId(Long gameId) {
+        List<ScoreBoard> scoreBoard = findScoreBoardByGameId(gameId);
+        String userTeamName = findUserTeamNameByGameId(gameId);
+
+        TeamScoreDTO teamScoreDTO = DTOConverter.scoreToTeamScoreDTO(scoreBoard.get(0));
+        TeamScoreDTO teamScoreDTO1 = DTOConverter.scoreToTeamScoreDTO(scoreBoard.get(1));
+
+        if (teamScoreDTO.getTeamName().equals(userTeamName)) {
+            return new GameScoreDTO(teamScoreDTO, teamScoreDTO1);
+        }
+        return new GameScoreDTO(teamScoreDTO1, teamScoreDTO);
+    }
+
     public List<ScoreBoard> findScoreBoardByGameId(Long gameId) {
         return boardDAO.findByGameId(gameId);
     }
@@ -36,4 +52,6 @@ public class GameService {
     public String findUserTeamNameByGameId(Long gameId) {
         return gameDAO.findUserTeamNameByGameId(gameId);
     }
+
+
 }
