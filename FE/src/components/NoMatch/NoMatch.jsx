ef36ce2aee5b4136426937abc60055e7c80e021c
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-10 23:53:34 +0900

    WIP: react login routing

--- FE/src/components/NoMatch/NoMatch.jsx
+++ FE/src/components/NoMatch/NoMatch.jsx
@@ -1,35 +1,11 @@
-import { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
-import Home from "../Home/Home";
+import React from "react";
 
 const NoMatch = () => {
-  const [isLoadHome, setIsLoadHome] = useState(false);
-  const loadHome = () => {
-    setIsLoadHome(true);
-  };
   return (
-    <Router>
-      {isLoadHome ? (
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      ) : (
-        <>
-          <div>잘못된 접근입니다. </div>
-          <button onClick={loadHome}>
-            <Link to="/">메인으로</Link>
-          </button>
-        </>
-      )}
-    </Router>
+    <>
+      <div>잘못된 접근입니다. </div>
+      <div>Error : 404</div>
+    </>
   );
 };
 
