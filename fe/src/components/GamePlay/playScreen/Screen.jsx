d62103c85747cc8355baba825e6439cac18655cf
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-10 17:25:46 +0900

    [ #19 ] style: screen  UI
    - style: screen.jsx 생성
    - style: background img 삽입

--- fe/src/components/GamePlay/playScreen/Screen.jsx
+++ fe/src/components/GamePlay/playScreen/Screen.jsx
@@ -9,4 +9,10 @@ export default Screen
 const StyledScreen = styled.section`
   width: 70%;
   height: 100%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url('https://upload.wikimedia.org/wikipedia/commons/8/80/Munhak_baseball_stadium_2012.png');
+  background-repeat: no-repeat;
+  background-size: cover;
 `
+
+const ScreenField  = styled.img``
