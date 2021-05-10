3e1c206fc1dbab04a9423412996441247482d112
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-11 03:01:02 +0900

    [#20]Style: ScreenRound  component 만듬

--- fe/src/components/GamePlay/playScreen/Screen.jsx
+++ fe/src/components/GamePlay/playScreen/Screen.jsx
@@ -4,6 +4,7 @@ const Screen = () => {
   return (
     <StyledScreen>
       <ScreenField src='field.svg' alt='field' />
+      <ScreenRound>2회 초 수비</ScreenRound>
     </StyledScreen>
   )
 }
@@ -19,9 +20,18 @@ const StyledScreen = styled.section`
     url('https://upload.wikimedia.org/wikipedia/commons/8/80/Munhak_baseball_stadium_2012.png');
   background-repeat: no-repeat;
   background-size: cover;
+  position: relative;
 `
 
 const ScreenField = styled.img`
   width: 34%;
   transform: rotate(45deg);
+`
+const ScreenRound = styled.span`
+  position: absolute;
+  right: 2%;
+  top: 2%;
+  color: ${props => props.theme.colors.white};
+  font-size: ${props => `${props.theme.fontSizes.LARGE}rem`};
+  font-weight: ${props => props.theme.weights.LARGE};
 `
