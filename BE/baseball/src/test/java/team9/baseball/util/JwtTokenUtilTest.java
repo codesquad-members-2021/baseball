3cca64c8036fc44b8192f5346edb4010efdc410d
정이삭 <isaac56@naver.com>
2021-05-10 22:43:10 +0900

    [#11] test: Create JwtTokenUtilTest

---
+++ BE/baseball/src/test/java/team9/baseball/util/JwtTokenUtilTest.java
@@ -0,0 +1,51 @@
+package team9.baseball.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class JwtTokenUtilTest {
+    @Test
+    @DisplayName("발급 및 인증 테스트")
+    void createParseTest() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", 1l);
+        map.put("userEmail", "isaac@naver.com");
+        map.put("resourceServer", "GITHUB");
+        String jwt = JwtTokenUtil.createToken("access", "user", map);
+        Claims claims = JwtTokenUtil.getTokenData(jwt);
+
+        Assertions.assertThat(claims.get("userId", Integer.class)).isEqualTo(1);
+        Assertions.assertThat(claims.get("userEmail", String.class)).isEqualTo("isaac@naver.com");
+        Assertions.assertThat(claims.get("resourceServer", String.class)).isEqualTo("GITHUB");
+    }
+
+    @Test
+    @DisplayName("서명 변조 테스트")
+    void signatureTest() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", 1l);
+        map.put("userEmail", "isaac@naver.com");
+        map.put("resourceServer", "GITHUB");
+        String jwt = JwtTokenUtil.createToken("access", "user", map);
+        String wrongJwt = jwt.substring(0, jwt.length() - 1);
+
+        checkWriteSignature(jwt, true);
+        checkWriteSignature(wrongJwt, false);
+    }
+
+    void checkWriteSignature(String jwt, boolean validated) {
+        boolean isWriteSignature = true;
+        try {
+            Claims claims = JwtTokenUtil.getTokenData(jwt);
+        } catch (SignatureException ex) {
+            isWriteSignature = false;
+        }
+        Assertions.assertThat(isWriteSignature).isEqualTo(validated);
+    }
+}
