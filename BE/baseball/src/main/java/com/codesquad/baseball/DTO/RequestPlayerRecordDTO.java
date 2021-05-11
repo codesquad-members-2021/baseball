4a804f0350194c170dc38683cbf3d701ad2ef030
Sally Oh <min27604@hotmail.com>
2021-05-11 20:32:15 +0900

    feat: 선수의 기록을 업데이트 할 때 받을 RequestPlayerRecordDTO 구현

---
+++ BE/baseball/src/main/java/com/codesquad/baseball/DTO/RequestPlayerRecordDTO.java
@@ -0,0 +1,28 @@
+package com.codesquad.baseball.DTO;
+
+public class RequestPlayerRecordDTO {
+
+    private Long id;
+
+    private int plateAppearance;
+
+    private int hit;
+
+    private int out;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getPlateAppearance() {
+        return plateAppearance;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public int getOut() {
+        return out;
+    }
+}
