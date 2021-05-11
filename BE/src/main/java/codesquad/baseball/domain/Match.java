64d4ff5b2d52f1d79feb505f2219f82eadba8f2c
rla36 <bhmarsd@naver.com>
2021-05-11 12:43:05 +0900

    feat: match에 홈팀, 원정팀 id를 리턴하는 메소드, 그리고 주어진 팀 id와 다른 id를 리턴하는 메소드 추가

--- BE/src/main/java/codesquad/baseball/domain/Match.java
+++ BE/src/main/java/codesquad/baseball/domain/Match.java
@@ -1,5 +1,6 @@
 package codesquad.baseball.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
@@ -24,4 +25,31 @@ public class Match {
     @NonNull
     private boolean isHome;
     //myteam 에 대해 isHome이 true일 경우, myTeam 수비 먼저, 공격 나중에. 초-수비/말-공격
+
+    @JsonIgnore
+    public Long getHomeTeamId() {
+        if(isHome) {
+            return myTeamId;
+        }
+        return counterTeamId;
+    }
+
+    @JsonIgnore
+    public Long getExpeditionTeamId() {
+        if(isHome) {
+            return counterTeamId;
+        }
+        return myTeamId;
+    }
+
+    @JsonIgnore
+    public Long getOtherTeamId(Long teamId) {
+        if(teamId.equals(counterTeamId)) {
+            return myTeamId;
+        }
+        if(teamId.equals(myTeamId)) {
+            return counterTeamId;
+        }
+        return -1L;
+    }
 }
