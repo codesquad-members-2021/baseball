d60da3112fa2acf1127f28940effef6264652372
DongHunKim <ultraraptor@naver.com>
2021-05-11 15:43:08 +0900

    ID와 Name , 숫자와 문자 모두로 Team을 검색하도록 변경

--- BE_baseball/src/main/java/com/dong/baseball/Repository/TeamRepository.java
+++ BE_baseball/src/main/java/com/dong/baseball/Repository/TeamRepository.java
@@ -2,9 +2,16 @@ package com.dong.baseball.Repository;
 
 import com.dong.baseball.Domain.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+//@Repository
 public interface TeamRepository extends CrudRepository<Team,Long> {
     List<Team> findAll();
+
+    Optional<Team> findByTeamId(Long teamId);
+
+    Optional<Team> findByTeamName(String teamName);
 }
