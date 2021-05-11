2c1f6374d17816b6e6c01245f37242b3679c5ea8
Sally Oh <min27604@hotmail.com>
2021-05-11 20:20:50 +0900

    feat: 선택한 팀의 상태에 따라 팀 정보를 반환하는 메서드 구현

--- BE/baseball/src/main/java/com/codesquad/baseball/service/TeamService.java
+++ BE/baseball/src/main/java/com/codesquad/baseball/service/TeamService.java
@@ -1,7 +1,12 @@
 package com.codesquad.baseball.service;
 
+import com.codesquad.baseball.DTO.RequestPlayerRecordDTO;
+import com.codesquad.baseball.DTO.TeamDTO;
+import com.codesquad.baseball.domain.Player;
+import com.codesquad.baseball.domain.Score;
 import com.codesquad.baseball.domain.Team;
 import com.codesquad.baseball.error.exception.TeamNotFoundException;
+import com.codesquad.baseball.error.exception.TeamNotPlayableException;
 import com.codesquad.baseball.repository.TeamRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +25,13 @@ public class TeamService {
 
     public Team browseTeamById(Long id) {
         return teamRepository.findById(id).orElseThrow(TeamNotFoundException::new);
+    }
+
+    public TeamDTO browseTeamStatus(Long id) {
+        Team team = browseTeamById(id);
+        if (!team.isPlayable()) {
+            throw new TeamNotPlayableException();
+        }
+        return TeamDTO.of(team);
     }
 }
