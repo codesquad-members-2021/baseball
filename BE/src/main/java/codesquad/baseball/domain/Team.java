e45470230e6968b808490403d50a99c7cc06b714
rla36 <bhmarsd@naver.com>
2021-05-11 12:38:23 +0900

    feat: team에게 투수, 첫 타자, 다음 타자를 물어보는 메소드 추가

--- BE/src/main/java/codesquad/baseball/domain/Team.java
+++ BE/src/main/java/codesquad/baseball/domain/Team.java
@@ -1,5 +1,6 @@
 package codesquad.baseball.domain;
 
+import codesquad.baseball.exception.PlayerNotFoundException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -39,7 +40,52 @@ public class Team {
             total += teamGameScore.getScore();
         }
         return total;
-    }//jung 실험 중
+    }
 
+    @JsonIgnore
+    public void initializeTotalScore(int inningNumber) {
+        TeamGameScore teamGameScore = new TeamGameScore(inningNumber, 0);
+    }
 
+    @JsonIgnore
+    public void setTotalScore(int inningNumber, int totalScore) {
+            teamGameScoreList.get(inningNumber-1).setScore(totalScore);
+    }
+
+    @JsonIgnore
+    public Player getNextPlayer(int currentPlayerOrder) {
+        try {
+            return playerList.get(currentPlayerOrder);
+        } catch (IndexOutOfBoundsException e) {
+            return getFirstHitter();
+        }
+    }
+
+    @JsonIgnore
+    public Player getPitcher() {
+        return playerList.stream()
+                .filter(Player::isPitcher)
+                .findFirst()
+                .orElseThrow(PlayerNotFoundException::new);
+    }
+
+    @JsonIgnore
+    public Player getFirstHitter() {
+        return playerList.get(0);
+    }
+
+    @JsonIgnore
+    public Player findPlayerByName(String name) {
+        return playerList.stream()
+                .filter(player -> player.getName().equals(name))
+                .findFirst()
+                .orElseThrow(PlayerNotFoundException::new);
+    }
+
+    @JsonIgnore
+    public void clearAllHistory() {
+        for (Player player : playerList) {
+            player.clearHistory();
+        }
+    }
 }
