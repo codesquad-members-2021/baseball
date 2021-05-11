7eefa89ee9146d037935640ed97e02b24f5bf0e1
DongHunKim <ultraraptor@naver.com>
2021-05-11 16:30:39 +0900

    배포스크립트 업데이트

--- BE_baseball/src/main/resources/deploy.sh
+++ BE_baseball/src/main/resources/deploy.sh
@@ -1,8 +1,9 @@
 # 설정
 LINE='#########################################################'
-SERVADDR='ec2-13-125-229-168.ap-northeast-2.compute.amazonaws.com' #배포할 AWS EC2 서버
-KEYFILE='~/Downloads/DongDong.pem' # 내 개발콤퓨터에서 AWS key 위치
+파SERVADDR='ec2-3-35-226-74.ap-northeast-2.compute.amazonaws.com' #배포할 AWS EC2 서버
+KEYFILE='~/Downloads/Dong210511.pem' # 내 개발콤퓨터에서 AWS key 위치
 AUTOINJECT_JAR=1 #1:자동으로 scp명령어로 밀어넣기, #0=안밀어넣고 명령어만 보여주고 끝내기
+NGINX_SETUP = '/Users/gimdonghun/Workspace/baseball/BE_baseball/src/main/resources/nginx.sh'
 #
 echo $LINE
 echo "## DongDong's 바이너리파일 밀어넣기 스크립트 "
@@ -18,6 +19,8 @@ echo $LINE
 
 if [ ${AUTOINJECT_JAR} -eq 1 ];then
     scp -i $KEYFILE $BINFILE ubuntu@$SERVADDR:/home/ubuntu
+    scp -i $KEYFILE $NGINX_SETUP ubuntu@$SERVADDR:/home/ubuntu
+
     echo "JAR FILE SEND OK...!"
 
     echo ""
