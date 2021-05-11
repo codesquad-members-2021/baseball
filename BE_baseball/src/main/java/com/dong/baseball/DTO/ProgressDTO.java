a1b83ac5ec6ea3fc046d893c5129b3946854e48a
dong <ultraraptor@naver.com>
2021-05-12 01:40:40 +0900

    Board 객체에서 ProgressDTO객체를 생성할 수 있도록 생성자 오버라이딩

--- BE_baseball/src/main/java/com/dong/baseball/DTO/ProgressDTO.java
+++ BE_baseball/src/main/java/com/dong/baseball/DTO/ProgressDTO.java
@@ -1,104 +1,70 @@
 package com.dong.baseball.DTO;
 
+import com.dong.baseball.Domain.Board;
 import com.dong.baseball.Domain.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProgressDTO {
-
+    private Long matchId;
     private String away;
     private String home;
     private int inning;
-    private String offenseTurn;
+    private String offenseTeam;
     private List<Player> firstBase;
     private List<Player> secondBase;
     private List<Player> thirdBase;
-    private Player pitcher;
-    private Player batter;
+    //private Player pitcher;
+    //private Player batter;
+    private String pitcher;
+    private String batter;
+    private int strike;
+    private int ball;
+    private int out;
+    private int homePoint;
+    private int awayPoint;
 
-
-    public ProgressDTO(String s) {
-        this.away = "away";
-        this.home = "home";
-        this.inning = 99;
-        this.offenseTurn = "offenseTurn";
-        this.firstBase = new ArrayList<>();
-        this.secondBase = new ArrayList<>();
-        this.thirdBase = new ArrayList<>();
-        this.pitcher = new Player();
-        this.batter = new Player();
-    }
-
-    public String getAway() {
-        return away;
-    }
-
-    public void setAway(String away) {
+    public ProgressDTO(Long matchId, String away, String home, int inning,
+                       String offenseTeam, List<Player> firstBase, List<Player> secondBase,
+                       List<Player> thirdBase, String pitcher, String batter, int strike,
+                       int ball, int out, int homePoint, int awayPoint) {
+        this.matchId = matchId;
         this.away = away;
-    }
-
-    public String getHome() {
-        return home;
-    }
-
-    public void setHome(String home) {
         this.home = home;
-    }
-
-    public int getInning() {
-        return inning;
-    }
-
-    public void setInning(int inning) {
         this.inning = inning;
-    }
-
-    public String getOffenseTurn() {
-        return offenseTurn;
-    }
-
-    public void setOffenseTurn(String offenseTurn) {
-        this.offenseTurn = offenseTurn;
-    }
-
-    public List<Player> getFirstBase() {
-        return firstBase;
-    }
-
-    public void setFirstBase(List<Player> firstBase) {
+        this.offenseTeam = offenseTeam;
         this.firstBase = firstBase;
-    }
-
-    public List<Player> getSecondBase() {
-        return secondBase;
-    }
-
-    public void setSecondBase(List<Player> secondBase) {
         this.secondBase = secondBase;
-    }
-
-    public List<Player> getThirdBase() {
-        return thirdBase;
-    }
-
-    public void setThirdBase(List<Player> thirdBase) {
         this.thirdBase = thirdBase;
-    }
-
-    public Player getPitcher() {
-        return pitcher;
-    }
-
-    public void setPitcher(Player pitcher) {
         this.pitcher = pitcher;
-    }
-
-    public Player getBatter() {
-        return batter;
-    }
-
-    public void setBatter(Player batter) {
         this.batter = batter;
+        this.strike = strike;
+        this.ball = ball;
+        this.out = out;
+        this.homePoint = homePoint;
+        this.awayPoint = awayPoint;
+    }
+
+    public ProgressDTO(Board board) {
+        this.matchId = board.getId();
+        this.away = "awayteam to string";
+        this.home = "hometeam to string";
+        this.inning = board.getInning();
+        this.offenseTeam = "offenseTeam to String";
+        //this.firstBase = firstBase;
+        //this.secondBase = secondBase;
+        //this.thirdBase = thirdBase;
+        this.firstBase = new ArrayList<>();
+        this.secondBase = new ArrayList<>();;
+        this.thirdBase = new ArrayList<>();;
+
+        this.pitcher = board.getPitcher();
+        this.batter = board.getBatter();
+        this.strike = board.getStrike();
+        this.ball = board.getBall();
+        this.out = board.getOut();
+        this.homePoint = board.getHomePoint();
+        this.awayPoint = board.getAwayPoint();
     }
 }
