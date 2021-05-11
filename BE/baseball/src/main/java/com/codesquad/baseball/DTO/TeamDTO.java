6ac43697a59d585638bee290f4f0fd5854f357ed
Sally Oh <min27604@hotmail.com>
2021-05-11 20:19:53 +0900

    feat: 팀 정보를 불러올 때 안의 players를 id값 순서에 맞게 정렬하도록 리스트로 변환하여 정렬

--- BE/baseball/src/main/java/com/codesquad/baseball/DTO/TeamDTO.java
+++ BE/baseball/src/main/java/com/codesquad/baseball/DTO/TeamDTO.java
@@ -1,37 +1,42 @@
 package com.codesquad.baseball.DTO;
 
-import com.codesquad.baseball.domain.Player;
 import com.codesquad.baseball.domain.Team;
 
-import java.util.Set;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TeamDTO {
 
-    private Long id;
+    private Long teamId;
 
     private String teamName;
 
-    private Set<Player> players;
+    private List<PlayerRecordDTO> players;
 
-    private TeamDTO(Long id, String name, Set<Player> players) {
-        this.id = id;
+    private TeamDTO(Long id, String name, List<PlayerRecordDTO> players) {
+        this.teamId = id;
         this.teamName = name;
         this.players = players;
     }
 
     public static TeamDTO of(Team team) {
-        return new TeamDTO(team.getId(), team.getName(), team.getPlayers());
+        List<PlayerRecordDTO> playerRecords = team.getPlayers().stream()
+                .map(PlayerRecordDTO::of)
+                .sorted(Comparator.comparing(PlayerRecordDTO::getId))
+                .collect(Collectors.toList());
+        return new TeamDTO(team.getId(), team.getName(), playerRecords);
     }
 
-    public Long getId() {
-        return id;
+    public Long getTeamId() {
+        return teamId;
     }
 
     public String getTeamName() {
         return teamName;
     }
 
-    public Set<Player> getPlayers() {
+    public List<PlayerRecordDTO> getPlayers() {
         return players;
     }
 }
