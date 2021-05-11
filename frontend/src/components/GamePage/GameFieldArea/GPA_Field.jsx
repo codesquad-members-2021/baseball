cdc0b156d11bafd74df16f0b2f934b89afa88762
Seohyoung <prophecy803@gmail.com>
2021-05-11 14:50:24 +0900

    [#24] feat: 타자 필드에서의 애니메이션 구현

--- frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
+++ frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
@@ -1,89 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { theme, Span } from '../../Style/Theme';
+import { ReactComponent as Field } from './Field.svg';
+import { ReactComponent as Ghost } from './ghost.svg';
+
 const GPA_Field = ({ type }) => {
-	console.log(type);
-	const handleClick = () => {
-		console.log('hi');
-	};
-	return (
-		<>
-			{type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
-			<Field_Area>
-				<svg
-					width="381"
-					height="363"
-					viewBox="0 0 381 363"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<rect
-						x="1.50072"
-						y="1.5"
-						width="317.288"
-						height="305.589"
-						transform="matrix(1 0 0.000482605 1 27.1845 27.9561)"
-						stroke="white"
-						stroke-width="3"
-					/>
-					<rect
-						width="59.2157"
-						height="57.0527"
-						rx="5"
-						transform="matrix(1 0 0.000482605 1 321.553 305.784)"
-						fill="white"
-					/>
-					<rect
-						width="59.2157"
-						height="57.0527"
-						rx="5"
-						transform="matrix(1 0 0.000483909 1 0.286499 0)"
-						fill="white"
-					/>
-					<rect
-						width="59.2157"
-						height="57.0527"
-						rx="5"
-						transform="matrix(1 0 0.000483909 1 317.898 0)"
-						fill="white"
-					/>
-					<path
-						d="M70.444 292.97L62.0914 329.939L31.8289 299.85L70.444 292.97Z"
-						fill="white"
-					/>
-					<rect
-						width="41.4964"
-						height="44.9802"
-						transform="matrix(0.709138 0.705069 -0.730955 0.682425 33.0296 299.535)"
-						fill="white"
-					/>
-				</svg>
-			</Field_Area>
-			<GameState>2회초 공격</GameState>
-		</>
-	);
+  const [move, setMove] = useState('');
+
+  const GhostSVG = styled(Ghost)`
+    position: absolute;
+    width: 3rem;
+    top: 52.5rem;
+    left: 23rem;
+    animation: run 2s forwards;
+    ${move}
+  `;
+
+  const handleClick = () => {
+    setMove(`
+    @keyframes run {
+      from {
+        transform: translateX(0rem) translateY(0rem);
+      }
+      to {
+        transform: translateX(15rem) translateY(-12.5rem);
+      }
+    }`);
+  };
+
+  return (
+    <>
+      {type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
+      <FieldArea>
+        <GameState>2회초 공격</GameState>
+        <FieldSVG />
+        <GhostSVG />
+      </FieldArea>
+    </>
+  );
 };
+
 const PITCH = styled.button`
-	position: relative;
-	top: 516px;
-	left: 243px;
-	cursor: pointer;
-	z-index: 9999;
-	background-color: ${theme.colors.transparent};
-	font-size: ${theme.fontSize.XX_large};
-	font-weight: ${theme.fontWeight.Bold};
-	color: ${theme.colors.white};
-	border: 5px solid ${theme.colors.white};
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
+  cursor: pointer;
+  z-index: 9999;
+  background-color: ${theme.colors.transparent};
+  font-size: ${theme.fontSize.XX_large};
+  font-weight: ${theme.fontWeight.Bold};
+  color: ${theme.colors.white};
+  border: 5px solid ${theme.colors.white};
 `;
-const Field_Area = styled.div`
-	transform: rotate(-45deg);
+const FieldArea = styled.div`
+  position: relative;
 `;
+
+const FieldSVG = styled(Field)`
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
+`;
+
 const GameState = styled(Span)`
-	position: relative;
-	top: -170px;
-	right: -750px;
-	font-size: ${theme.fontSize.X_large};
-	font-weight: ${theme.fontWeight.light};
-	color: ${theme.colors.white};
+  position: absolute;
+  top: 17rem;
+  right: 23rem;
+  font-size: ${theme.fontSize.X_large};
+  font-weight: ${theme.fontWeight.light};
+  color: ${theme.colors.white};
 `;
 export default GPA_Field;
--- frontend/src/components/StartPage/MatchingInfo.jsx
+++ frontend/src/components/StartPage/MatchingInfo.jsx
@@ -2,63 +2,81 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import useFetch from '../Hook/useFetch';
 import { theme } from '../Style/Theme';
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const MatchingInfo = ({ setMessage, data }) => {
-	const [currentID, setID] = useState(null);
-	const [occupiedState, loadingOccupiedState, occupied] = useFetch(
-		'patch',
-		'initGame',
-		currentID,
-	);
+  const [currentID, setID] = useState(null);
+  const [occupiedState, loadingOccupiedState, occupied] = useFetch(
+    'patch',
+    'initGame',
+    currentID
+  );
 
-	const history = useHistory();
-	const handleClick = useCallback(
-		async (id, type) => {
-			await setID(id);
-			console.log(occupiedState);
-			if (!occupied && type === 'HOME') {
-				history.push(`/defense/${id}`);
-			} else if (!occupied && type === 'AWAY') {
-				history.push(`/attack/${id}`);
-			} else {
-				//occupied=true인경우(409error)
-				setMessage(`이미 게임이 시작되었습니다. \n다른 팀을 선택해주세요`);
-				console.log(data);
-			}
-		},
-		[history],
-	);
-	return (
-		<TeamWrapper>
-			<TeamName onClick={() => handleClick(data.id, 'AWAY')}>
-				{data.awayTeam.teamName}
-			</TeamName>
-			<VS>VS</VS>
-			<TeamName onClick={() => handleClick(data.id, 'HOME')}>
-				{data.homeTeam.teamName}
-			</TeamName>
-		</TeamWrapper>
-	);
+  const history = useHistory();
+
+  const handleClick = (id, type) => {
+    // setID(id);
+    if (occupied && type === 'HOME') {
+      history.push(`/defense/${id}`);
+    } else if (occupied && type === 'AWAY') {
+      history.push(`/attack/${id}`);
+    } else {
+      setMessage(`이미 게임이 시작되었습니다. \n다른 팀을 선택해주세요`);
+      //occupied=true인경우(409error)
+    }
+  };
+
+  const TeamStatus = occupied => {
+    return (
+      <>
+        <TeamName
+          className={occupied ? 'occupied' : ''}
+          onClick={() => {
+            handleClick(data.id, 'AWAY');
+          }}
+        >
+          {data.awayTeam.teamName}
+        </TeamName>
+        <VS>VS</VS>
+        <TeamName
+          className={occupied ? 'occupied' : ''}
+          onClick={() => handleClick(data.id, 'HOME')}
+        >
+          {data.homeTeam.teamName}
+        </TeamName>
+      </>
+    );
+  };
+
+  return (
+    <TeamWrapper>
+      {data.occupied ? <TeamStatus occupied /> : <TeamStatus />}
+    </TeamWrapper>
+  );
 };
 const VS = styled.div`
-	font-size: ${theme.fontSize.large};
-	font-weight: ${theme.fontWeight.bold};
-	color: ${theme.colors.grey_deep};
+  font-size: ${theme.fontSize.large};
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.grey_deep};
 `;
 const TeamName = styled.span`
-	font-size: ${theme.fontSize.X_large};
-	font-weight: ${theme.fontWeight.bold};
-	cursor: pointer;
-	&:hover {
-		color: ${theme.colors.red};
-	}
+  font-size: ${theme.fontSize.X_large};
+  font-weight: ${theme.fontWeight.bold};
+  cursor: pointer;
+  &:hover {
+    color: ${theme.colors.red};
+  }
+
+  .occupied {
+    pointer-events: none;
+    color: ${theme.colors.grey_deep};
+  }
 `;
 const TeamWrapper = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-around;
-	padding: 0 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 30px;
 `;
 
 export default MatchingInfo;
