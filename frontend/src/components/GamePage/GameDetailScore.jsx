1e3a5e05ec60bcd996173ad6e17afae8b8d28ff2
junamee <skawn11114@gmail.com>
2021-05-11 14:54:43 +0900

    [#39]Feat. slide 창 전환되기

--- frontend/src/components/GamePage/GameDetailScore.jsx
+++ frontend/src/components/GamePage/GameDetailScore.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
+import styled from 'styled-components';
+import { Span, theme } from '../Style/Theme';
 const GameDetailScore = () => {
-	return <div></div>;
+	return <DetailWrapper>상세점수현황</DetailWrapper>;
 };
-
+const DetailWrapper = styled(Span)`
+	color: ${theme.colors.white};
+`;
 export default GameDetailScore;
---
+++ frontend/src/components/GamePage/GameGrid.jsx
@@ -0,0 +1,41 @@
+import useFetch from '../Hook/useFetch';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import GameGeneralInfo from './GameGeneralInfo';
+import GamePlayers from './GamePlayers';
+import GameFieldArea from './GameFieldArea';
+import GameLog from './GameLog';
+import GamePlayersList from './GamePlayersList';
+import GameDetailScore from './GameDetailScore';
+import useSlide from '../Hook/useSlide';
+
+const GameGrid = ({ data, type }) => {
+	const gameId = data.id;
+	const [gameInfo, loading, error] = useFetch('patch', 'initGame', gameId);
+
+	return (
+		<>
+			{loading ? (
+				console.log('Loading...')
+			) : (
+				<GridBox>
+					<GameGeneralInfo data={gameInfo}></GameGeneralInfo>
+					<GamePlayers />
+					<GameFieldArea type={type} />
+					<GameLog data={gameInfo}></GameLog>
+				</GridBox>
+			)}
+		</>
+	);
+};
+
+const GridBox = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	grid-template-columns: 80% 20%;
+	grid-template-rows: 20% 80%;
+	grid-gap: 5px;
+`;
+
+export default GameGrid;
