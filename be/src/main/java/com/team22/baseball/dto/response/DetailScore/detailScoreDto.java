3156b5105836f7f6afa0c78b4b58f7946da1ea2d
sanhee <68936@naver.com>
2021-05-11 11:52:09 +0900

    [#14] fix: 팀 선택 API 기존 요구사항 형식에 맞게 개선

--- be/src/main/java/com/team22/baseball/dto/response/DetailScore/detailScoreDto.java
+++ be/src/main/java/com/team22/baseball/dto/response/DetailScore/detailScoreDto.java
@@ -3,6 +3,7 @@ package com.team22.baseball.dto.response.DetailScore;
 import com.team22.baseball.domain.TeamScore;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class detailScoreDto {
@@ -20,8 +21,18 @@ public class detailScoreDto {
         return name;
     }
 
-    public List<TeamScore> getScores() {
-        return scores;
+    public int[] getScores() {
+
+        //INFO. SCORES 는 라운드 정보를 포함하고 있는 동적 객체여서 그대로 보낼 수도 있지만, 프론트단에서 동적으로 파싱하기 불편하여 12 사이즈의 배열로 변환함
+
+        int[] arrayScore = new int[10];
+        Arrays.fill(arrayScore, -1); // 점수가 없는건 -1
+
+        for (int i = 0; i < scores.size(); i++) {
+            arrayScore[i] = scores.get(i).getScore();
+        }
+
+        return arrayScore;
     }
 
     public void setName(String name) {
