b3be8983954b9c1548552786df3ff2e6e7028d47
DongHunKim <ultraraptor@naver.com>
2021-05-11 15:43:19 +0900

    ID와 Name , 숫자와 문자 모두로 Team을 검색하도록 변경

--- BE_baseball/src/main/java/com/dong/baseball/Service/TeamService.java
+++ BE_baseball/src/main/java/com/dong/baseball/Service/TeamService.java
@@ -1,9 +1,12 @@
 package com.dong.baseball.Service;
 
+import com.dong.baseball.DTO.TeamRankDTO;
 import com.dong.baseball.Domain.Team;
+import com.dong.baseball.Exception.TeamNotFound;
 import com.dong.baseball.Repository.TeamRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -18,21 +21,29 @@ public class TeamService {
     }
 
 
-    public List<Team> teamRanking() {
+    public List<TeamRankDTO> teamRanking() {
         List<Team> teamList = teamRepository.findAll();
+        System.out.println("@@@@@@@@" + teamList + "  : " + teamList.size());
+        List<TeamRankDTO> teamRankDTOList = new ArrayList<>();
 
-        Collections.sort(teamList, new Comparator<Team>() {
+        for(Team team : teamList) {
+            teamRankDTOList.add(new TeamRankDTO(team));
+        }
+
+        teamRankDTOList.sort(new Comparator<TeamRankDTO>() {
             @Override
-            public int compare(Team t1, Team t2) {
-                if (t1.getVictoryPoint() > t2.getVictoryPoint()) {
-                    return -1;
-                }
-                if (t1.getVictoryPoint() < t2.getVictoryPoint()) {
-                    return 1;
-                }
-                return 0;
+            public int compare(TeamRankDTO t1, TeamRankDTO t2) {
+                return Integer.compare(t2.getVictoryPoint(), t1.getVictoryPoint());
             }
         });
-        return teamList;
+        return teamRankDTOList;
+    }
+
+    public Team teamInfoById(Long teamId) {
+        return teamRepository.findByTeamId(teamId).orElseThrow (() -> new TeamNotFound());
+    }
+
+    public Team teamInfoByName(String teamName) {
+        return teamRepository.findByTeamName(teamName).orElseThrow(() -> new TeamNotFound());
     }
 }
