28714a3ec98de31af1f8528be651459a896d23e2
DongHunKim <ultraraptor@naver.com>
2021-05-11 15:40:23 +0900

    Optional 사용에 대응하여 커스텀 익셉션 추가

---
+++ BE_baseball/src/main/java/com/dong/baseball/Exception/PlayerNotFound.java
@@ -0,0 +1,9 @@
+package com.dong.baseball.Exception;
+
+public class PlayerNotFound extends RuntimeException{
+    static final String MESSAGE = "Palyer Cannot founded";
+
+    public PlayerNotFound() {
+
+    }
+}
