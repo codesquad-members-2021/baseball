127e9a40776ac2044cfb80a078dbbbc0f855eb14
gisungchoi <chlrltjd159@naver.com>
2021-05-12 21:43:29 +0900

    feat : secretkey를 관리 하기 위한 class 정의

---
+++ BE/src/main/java/com/codesquad/coco/user/domain/SecretServerKey.java
@@ -0,0 +1,26 @@
+package com.codesquad.coco.user.domain;
+
+import java.nio.charset.StandardCharsets;
+
+public class SecretServerKey {
+
+    private String clientId;
+    private String clientSecret;
+    private String serverSecret;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public String getServerSecret() {
+        return serverSecret;
+    }
+
+    public byte[] serverSecretToByteArray() {
+        return serverSecret.getBytes(StandardCharsets.UTF_8);
+    }
+}
---
+++ BE/src/main/java/com/codesquad/coco/utils/FindSecretServerKey.java
@@ -0,0 +1,21 @@
+package com.codesquad.coco.utils;
+
+import com.codesquad.coco.user.domain.SecretServerKey;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FindSecretServerKey {
+
+    public static SecretServerKey find() {
+        try {
+            InputStream inputStream = FindSecretServerKey.class.getResourceAsStream("/server.secret.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(inputStream, SecretServerKey.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
