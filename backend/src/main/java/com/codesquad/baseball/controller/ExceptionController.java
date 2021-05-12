e4bb60b92058f1de8a4dcc38c0420b1e0d9a4bd4
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-12 11:34:28 +0900

    [#36] feat: Method Not Allowed에 대한 예외처리 추가

--- backend/src/main/java/com/codesquad/baseball/controller/ExceptionController.java
+++ backend/src/main/java/com/codesquad/baseball/controller/ExceptionController.java
@@ -3,6 +3,7 @@ package com.codesquad.baseball.controller;
 import com.codesquad.baseball.dto.ErrorDTO;
 import com.codesquad.baseball.exceptions.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,5 +21,11 @@ public class ExceptionController {
     @ExceptionHandler({PlayerNotFoundException.class, TeamNotFoundException.class, GameNotFoundException.class})
     public ErrorDTO handlePlayerNotFoundException(NotFoundException exception) {
         return new ErrorDTO(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ErrorDTO handleInternalServerError(HttpRequestMethodNotSupportedException exception) {
+        return new ErrorDTO(HttpStatus.METHOD_NOT_ALLOWED, exception.getMessage());
     }
 }
