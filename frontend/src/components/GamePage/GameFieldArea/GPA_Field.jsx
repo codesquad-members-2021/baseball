9448113f1f976f6305ef6a82f78ebdfe21063e64
Seohyoung <prophecy803@gmail.com>
2021-05-13 02:13:23 +0900

    [#33] feat : 현재 타자, 포수 정보 우측 상단 표시
    - 현재 사용자의 팀이 공격인 경우만 구현한 상태이므로 추가 구현 필요

--- frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
+++ frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
@@ -7,27 +7,26 @@ import API from '../../Hook/API';
 import { useGameState, useDispatch } from '../../GameContext';
 
 const GPA_Field = ({ type, gameId }) => {
-	const { state } = useGameState();
-	const [move, setMove] = useState('');
-	const dispatch = useDispatch();
-	const GhostSVG = styled(Ghost)`
-		position: absolute;
-		width: 3rem;
-		top: 52.5rem;
-		left: 23rem;
-		animation: run 2s forwards;
-		${move}
-	`;
+  const { state } = useGameState();
+  const [move, setMove] = useState('');
+  const dispatch = useDispatch();
+  const GhostSVG = styled(Ghost)`
+    position: absolute;
+    width: 3rem;
+    top: 52.5rem;
+    left: 23rem;
+    animation: run 2s forwards;
+    ${move}
+  `;
 
-	const handleClick = () => {
-		const pitchResult = async () => {
-			const response = await API.post.pitch(gameId);
-			dispatch({ type: 'pitch', payload: response });
-			console.log(state);
-		};
-		pitchResult();
+  const handleClick = () => {
+    const pitchResult = async () => {
+      const response = await API.post.pitch(gameId);
+      dispatch({ type: 'pitch', payload: response });
+    };
+    pitchResult();
 
-		setMove(`
+    setMove(`
     @keyframes run {
       from {
         transform: translateX(0rem) translateY(0rem);
@@ -36,48 +35,48 @@ const GPA_Field = ({ type, gameId }) => {
         transform: translateX(15rem) translateY(-12.5rem);
       }
     }`);
-	};
+  };
 
-	return (
-		<>
-			{type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
-			<FieldArea>
-				<GameState>2회초 공격</GameState>
-				<FieldSVG />
-				<GhostSVG />
-			</FieldArea>
-		</>
-	);
+  return (
+    <>
+      {type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
+      <FieldArea>
+        <GameState>2회초 공격</GameState>
+        <FieldSVG />
+        <GhostSVG />
+      </FieldArea>
+    </>
+  );
 };
 
 const PITCH = styled.button`
-	position: absolute;
-	top: 23rem;
-	left: 3rem;
-	cursor: pointer;
-	z-index: 9999;
-	background-color: ${theme.colors.transparent};
-	font-size: ${theme.fontSize.XX_large};
-	font-weight: ${theme.fontWeight.Bold};
-	color: ${theme.colors.white};
-	border: 5px solid ${theme.colors.white};
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
+  cursor: pointer;
+  z-index: 9999;
+  background-color: ${theme.colors.transparent};
+  font-size: ${theme.fontSize.XX_large};
+  font-weight: ${theme.fontWeight.Bold};
+  color: ${theme.colors.white};
+  border: 5px solid ${theme.colors.white};
 `;
 const FieldArea = styled.div`
-	position: relative;
+  position: relative;
 `;
 
 const FieldSVG = styled(Field)`
-	position: absolute;
-	top: 23rem;
-	left: 3rem;
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
 `;
 
 const GameState = styled(Span)`
-	position: absolute;
-	top: 17rem;
-	right: 23rem;
-	font-size: ${theme.fontSize.X_large};
-	font-weight: ${theme.fontWeight.light};
-	color: ${theme.colors.white};
+  position: absolute;
+  top: 17rem;
+  right: 23rem;
+  font-size: ${theme.fontSize.X_large};
+  font-weight: ${theme.fontWeight.light};
+  color: ${theme.colors.white};
 `;
 export default GPA_Field;
--- frontend/src/components/GamePage/GamePlayers.jsx
+++ frontend/src/components/GamePage/GamePlayers.jsx
@@ -4,45 +4,80 @@ import { theme } from '../Style/Theme';
 import { useGameState } from '../GameContext';
 
 const GamePlayers = () => {
-	const { state } = useGameState();
+  const { state } = useGameState();
 
-	return (
-		<PlayerWrapper>
-			<Team>
-				<Role>투수</Role>
-				<Name>최동원</Name>
-				<State>#39</State>
-			</Team>
-			<Team>
-				<Role>타자</Role>
-				<Name>둥이</Name>
-				<State> 1타석 0안타</State>
-			</Team>
-		</PlayerWrapper>
-	);
+  const TeamPlayersInfo = () => {
+    return state.gameStatusDTO ? (
+      <>
+        <Team>
+          <Role>투수</Role>
+          <Name>
+            {state.homeTeam.players[state.gameStatusDTO.currentPitcher].name}
+          </Name>
+          <State>
+            {state.homeTeam.players[state.gameStatusDTO.currentPitcher].id}
+          </State>
+        </Team>
+        <Team>
+          <Role>타자</Role>
+          <Name>
+            {state.awayTeam.players[state.gameStatusDTO.currentHitter].name}
+          </Name>
+          <State>
+            {
+              state.awayTeam.players[state.gameStatusDTO.currentHitter]
+                .plateAppearances
+            }
+            타석
+            {state.awayTeam.players[state.gameStatusDTO.currentHitter].hitCount}
+            안타
+          </State>
+        </Team>
+      </>
+    ) : (
+      <>
+        <Team>
+          <Role>투수</Role>
+          <Name>{state.homeTeam.players[0].name}</Name>
+          <State>{state.homeTeam.players[0].id}</State>
+        </Team>
+        <Team>
+          <Role>타자</Role>
+          <Name>{state.awayTeam.players[0].name}</Name>
+          <State>0타석 0안타</State>
+        </Team>
+      </>
+    );
+  };
+
+  return (
+    <PlayerWrapper>
+      <TeamPlayersInfo />
+    </PlayerWrapper>
+  );
 };
 const PlayerWrapper = styled.div`
-	border-bottom: 5px solid ${theme.colors.white};
-	box-sizing: border-box;
+  border-bottom: 5px solid ${theme.colors.white};
+  box-sizing: border-box;
 `;
 const Team = styled.div`
-	margin: 20px;
-	box-sizing: border-box;
+  margin: 20px;
+  box-sizing: border-box;
 `;
 const Role = styled.span`
-	font-size: ${theme.fontSize.large};
-	font-weight: ${theme.fontWeight.bold};
-	color: ${theme.colors.white};
+  font-size: ${theme.fontSize.large};
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.white};
 `;
 const Name = styled.span`
-	margin-left: 10px;
-	font-size: ${theme.fontSize.medium};
-	font-weight: ${theme.fontWeight.bold};
-	color: ${theme.colors.skyblue_log};
+  margin-left: 10px;
+  font-size: ${theme.fontSize.medium};
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.skyblue_log};
 `;
 const State = styled.div`
-	font-size: ${theme.fontSize.medium};
-	font-weight: ${theme.fontWeight.normal};
-	color: ${theme.colors.blue_log};
+  font-size: ${theme.fontSize.medium};
+  font-weight: ${theme.fontWeight.normal};
+  color: ${theme.colors.blue_log};
 `;
 export default GamePlayers;
