3d11e82bd490cd53ea7089f4a1525c47c75388fd
gisungchoi <chlrltjd159@naver.com>
2021-05-12 21:41:31 +0900

    feat : github uri 정의
    - enum으로 관리 한다

---
+++ BE/src/main/java/com/codesquad/coco/user/domain/GitURI.java
@@ -0,0 +1,19 @@
+package com.codesquad.coco.user.domain;
+
+public enum GitURI {
+
+    REDIRECT_URI("http://localhost:8080/login/github"),
+    ACCESS_TOKEN_URI("https://github.com/login/oauth/access_token"),
+    AUTHORIZE_URI("https://github.com/login/oauth/authorize"),
+    USER_INFO_URI("https://api.github.com/user");
+
+    private String uri;
+
+    GitURI(String uri) {
+        this.uri = uri;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+}
