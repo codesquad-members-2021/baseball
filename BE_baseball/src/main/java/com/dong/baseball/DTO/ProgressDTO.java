cb02d829c6abcbc1daa52115380938d65b00076b
dong <ultraraptor@naver.com>
2021-05-12 15:24:15 +0900

    getter setter 추가

--- BE_baseball/src/main/java/com/dong/baseball/DTO/ProgressDTO.java
+++ BE_baseball/src/main/java/com/dong/baseball/DTO/ProgressDTO.java
@@ -52,6 +52,7 @@ public class ProgressDTO {
         this.home = "hometeam to string";
         this.inning = board.getInning();
         this.offenseTeam = "offenseTeam to String";
+
         //this.firstBase = firstBase;
         //this.secondBase = secondBase;
         //this.thirdBase = thirdBase;
@@ -66,5 +67,146 @@ public class ProgressDTO {
         this.out = board.getOut();
         this.homePoint = board.getHomePoint();
         this.awayPoint = board.getAwayPoint();
+    }
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+
+    public String getAway() {
+        return away;
+    }
+
+    public void setAway(String away) {
+        this.away = away;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public int getInning() {
+        return inning;
+    }
+
+    public void setInning(int inning) {
+        this.inning = inning;
+    }
+
+    public String getOffenseTeam() {
+        return offenseTeam;
+    }
+
+    public void setOffenseTeam(String offenseTeam) {
+        this.offenseTeam = offenseTeam;
+    }
+
+    public List<Player> getFirstBase() {
+        return firstBase;
+    }
+
+    public void setFirstBase(List<Player> firstBase) {
+        this.firstBase = firstBase;
+    }
+
+    public List<Player> getSecondBase() {
+        return secondBase;
+    }
+
+    public void setSecondBase(List<Player> secondBase) {
+        this.secondBase = secondBase;
+    }
+
+    public List<Player> getThirdBase() {
+        return thirdBase;
+    }
+
+    public void setThirdBase(List<Player> thirdBase) {
+        this.thirdBase = thirdBase;
+    }
+
+    public String getPitcher() {
+        return pitcher;
+    }
+
+    public void setPitcher(String pitcher) {
+        this.pitcher = pitcher;
+    }
+
+    public String getBatter() {
+        return batter;
+    }
+
+    public void setBatter(String batter) {
+        this.batter = batter;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getOut() {
+        return out;
+    }
+
+    public void setOut(int out) {
+        this.out = out;
+    }
+
+    public int getHomePoint() {
+        return homePoint;
+    }
+
+    public void setHomePoint(int homePoint) {
+        this.homePoint = homePoint;
+    }
+
+    public int getAwayPoint() {
+        return awayPoint;
+    }
+
+    public void setAwayPoint(int awayPoint) {
+        this.awayPoint = awayPoint;
+    }
+
+    @Override
+    public String toString() {
+        return "ProgressDTO{" +
+                "matchId=" + matchId +
+                ", away='" + away + '\'' +
+                ", home='" + home + '\'' +
+                ", inning=" + inning +
+                ", offenseTeam='" + offenseTeam + '\'' +
+                ", firstBase=" + firstBase +
+                ", secondBase=" + secondBase +
+                ", thirdBase=" + thirdBase +
+                ", pitcher='" + pitcher + '\'' +
+                ", batter='" + batter + '\'' +
+                ", strike=" + strike +
+                ", ball=" + ball +
+                ", out=" + out +
+                ", homePoint=" + homePoint +
+                ", awayPoint=" + awayPoint +
+                '}';
     }
 }
