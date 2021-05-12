babc1121f77bf3cb110dfeccbbdf61ff3a08d983
malaheaven <yunhg95@gmail.com>
2021-05-12 22:19:05 +0900

    test: MatchInfo 3가지 시나리오 테스트 추가
    - 타자가 바뀌면, ball, strike, playerResults 는 초기화 되어야하지만, out 은 초기화 되면 안된다.
    - 공수가 바뀌면, MatchInfo 가 halvesCount 를 제외하고 초기화 되야 한다.
    - 공수가 2번 바뀌면, 이닝이 증가해야 한다.

--- be/server/src/test/java/com/baseball/domain/match/MatchInfoTest.java
+++ be/server/src/test/java/com/baseball/domain/match/MatchInfoTest.java
@@ -207,18 +207,51 @@ class MatchInfoTest {
     @Test
     @DisplayName("타자가 바뀌면, ball, strike, playerResults 는 초기화 되어야하지만, out 은 초기화 되면 안된다.")
     void proceedToNextBase_reset() {
-        // TODO:
+        for (int i = 0; i < 3; i++) {
+            matchInfo.pushPlayResult(STRIKE);
+        }
+        matchInfo.proceedToNextBase();
+        softly.assertThat(matchInfo.getHalvesCount())
+                .isEqualTo(1);
+        softly.assertThat(matchInfo.getStrikeCount())
+                .isEqualTo(0);
+        softly.assertThat(matchInfo.getBallCount())
+                .isEqualTo(0);
+        softly.assertThat(matchInfo.getOutCount())
+                .isEqualTo(1);
+        softly.assertThat(matchInfo.getBases())
+                .isEqualTo(Arrays.asList(true, false, false));
+        softly.assertThat(matchInfo.getPitcherInfo())
+                .isEqualTo(new ArrayList<>());
+        softly.assertAll();
     }
 
     @Test
     @DisplayName("공수가 바뀌면, MatchInfo 가 halvesCount 를 제외하고 초기화 되야 한다.")
     void proceedToNextHalve_reset() {
-        // TODO:
+        matchInfo.proceedToNextHalve();
+        softly.assertThat(matchInfo.getHalvesCount())
+                .isEqualTo(2);
+        softly.assertThat(matchInfo.getStrikeCount())
+                .isEqualTo(0);
+        softly.assertThat(matchInfo.getBallCount())
+                .isEqualTo(0);
+        softly.assertThat(matchInfo.getOutCount())
+                .isEqualTo(0);
+        softly.assertThat(matchInfo.getBases())
+                .isEqualTo(Arrays.asList(false, false, false));
+        softly.assertThat(matchInfo.getPitcherInfo())
+                .isEqualTo(new ArrayList<>());
+        softly.assertAll();
     }
 
     @Test
     @DisplayName("공수가 2번 바뀌면, 이닝이 증가해야 한다.")
     void proceedToNextHalve_increaseInning() {
-        // TODO:
+        for (int i = 0; i < 2; i++) {
+            matchInfo.proceedToNextHalve();
+        }
+        softly.assertThat(matchInfo.getInningCount()).isEqualTo(2);
+        softly.assertAll();
     }
 }
