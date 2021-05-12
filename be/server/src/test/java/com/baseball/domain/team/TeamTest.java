1a1ee77e3aeb4f3b99c0508dff052c2ba84f22e3
malaheaven <yunhg95@gmail.com>
2021-05-12 20:40:16 +0900

    test: TeamTest 5가지 상황 구현
    - pushScore 를 하면, 팀에 값이 0인 점수판이 추가되어야 한다.
    - increaseScore 를 하면, 팀의 마지막 점수판이 증가되어야 한다.
    - changePitcher 를 하면, 다음 투수로 변경되어야 한다.
    - playOffense 를 하면, HIT 일 경우 다음 타자로 변경되어야 한다.
    - playDefense 를 하면, 투수가 play 를 한 것과 동일한 효과가 나타나야 한다.

--- be/server/src/test/java/com/baseball/domain/team/TeamTest.java
+++ be/server/src/test/java/com/baseball/domain/team/TeamTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static com.baseball.domain.match.PlayResult.HIT;
+import static com.baseball.domain.match.PlayResult.STRIKE;
 import static com.baseball.domain.team.TeamFactory.createTeam;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TeamTest {
     private Team team;
@@ -17,30 +22,45 @@ class TeamTest {
     @Test
     @DisplayName("pushScore 를 하면, 팀에 값이 0인 점수판이 추가되어야 한다.")
     void pushScore() {
-        // TODO:
+        team.pushScore();
+        assertThat(team.getScores()).isEqualTo(Arrays.asList(0));
+
     }
 
     @Test
     @DisplayName("increaseScore 를 하면, 팀의 마지막 점수판이 증가되어야 한다.")
     void increaseScore() {
-        // TODO:
+        team.pushScore();
+        team.increaseScore();
+        assertThat(team.getScores()).isEqualTo(Arrays.asList(1));
     }
 
     @Test
     @DisplayName("changePitcher 를 하면, 다음 투수로 변경되어야 한다.")
     void changePitcher() {
-        // TODO:
+        assertThat(team.getPitcher().getName()).isEqualTo("TEAM1투수");
+
+        team.changePitcher();
+        assertThat(team.getPitcher().getName()).isEqualTo("TEAM2투수");
+
     }
 
     @Test
     @DisplayName("playOffense 를 하면, HIT 일 경우 다음 타자로 변경되어야 한다.")
     void playOffense() {
-        // TODO:
+        assertThat(team.getBatter().getName()).isEqualTo("TEAM1타자");
+
+        team.playOffense(HIT);
+        assertThat(team.getBatter().getName()).isEqualTo("TEAM2타자");
     }
 
     @Test
     @DisplayName("playDefense 를 하면, 투수가 play 를 한 것과 동일한 효과가 나타나야 한다.")
     void playDefense() {
-        // TODO:
+        team.playDefense(STRIKE);
+        assertThat(team.getPitcher().getNumberOfPitching()).isEqualTo(1);
+
+        team.getPitcher().play(STRIKE);
+        assertThat(team.getPitcher().getNumberOfPitching()).isEqualTo(2);
     }
 }
