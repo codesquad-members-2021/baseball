b630ee2c7f70da0196bcb17ebdc9b37c61b2dabf
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2021-05-13 22:46:54 +0900

    style:PopUp refactor

--- fe/src/components/GamePlay/playPopUp/PopUp.jsx
+++ fe/src/components/GamePlay/playPopUp/PopUp.jsx
@@ -1,64 +1,26 @@
-import styled, { keyframes } from 'styled-components'
-import { useState } from 'react'
+import styled from 'styled-components'
+
 const PopUp = ({ children, type }) => {
-  const [active, setActive] = useState(false)
-
-  return active ? (
-    <StyledPopUp onClick={() => setActive(false)}>
-      <PopUpWarpper {...{ type }}>{children}</PopUpWarpper>
-    </StyledPopUp>
-  ) : (
-    <WaitingPopUp {...{ type }} onMouseEnter={() => setActive(true)} />
-  )
+  return <StyledPopUp {...{ type }}>{children}</StyledPopUp>
 }
-
 export default PopUp
-
-const popUpAnimation = keyframes`
-0% {
-  transform: translateY(-5rem);
-  opacity: 0;
-}
-100%{
-  transform: translateY(0);
-  opacity: 1;
-}
-
-`
-const popDownAnimation = keyframes`
-0% {
-  transform: translateY(5rem); 
-  opacity: 0;
-100%{
-  transform: translateY(0);
-  opacity: 1;
-}
-
-`
-
-const PopUpWarpper = styled.div`
-  animation: ${({ type }) => {
-      if (type === 'up') return popUpAnimation
-      else return popDownAnimation
-    }}
-    0.3s ease-in;
-`
 
 const StyledPopUp = styled.section`
   position: fixed;
   z-index: 9999;
   width: 100%;
   height: 100%;
+  top: ${({ type }) => (type === 'up' ? '-95%' : '95%')};
+  transition: .5s ease-out;
   background-color: rgba(0, 0, 0, 0.8);
-`
-const WaitingPopUp = styled.section`
-  position: fixed;
-  z-index: 9999;
-  width: 100%;
-  height: 5%;
-  ${({ type }) => {
-    if (type === 'up') return 'top: 0'
-    else return 'bottom: 0'
-  }};
-  background-color: red;
+  opacity: 0;
+  &:hover {
+    top: 0;
+    opacity: 1;
+  }
+  &:active {
+    transition: none;
+    top: ${({ type }) => (type === 'up' ? '-95%' : '95%')};
+    opacity: 0;
+  }
 `
