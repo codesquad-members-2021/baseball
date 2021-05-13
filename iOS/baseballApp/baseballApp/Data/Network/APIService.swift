2e5da292ba04e398d701896512147cf6012dec3b
Ray suh <readingod@naver.com>
2021-05-13 17:46:49 +0900

    깃충돌해결

--- iOS/baseballApp/baseballApp/Data/Network/APIService.swift
+++ iOS/baseballApp/baseballApp/Data/Network/APIService.swift
@@ -4,7 +4,7 @@ import Alamofire
 
 class APIService {
     static let shared: APIService = APIService()
-
+    
     func requestGames() throws -> Observable<GameDTO> {
         guard  let url = Endpoint.getGames(path: "baseball") else {
             throw APIError.urlNotSupport
@@ -12,18 +12,21 @@ class APIService {
         return getAndDecode(url)
     }
     
-
+    
     func requestPlay(gameID: Int, inningID: Int) throws -> Observable<PlayDTO> {
-        guard  let url = Endpoint.getGames(path: "baseball/\(gameID)/inning/\(inningID)") else {
-
+        guard  let url = Endpoint.getGames(path: "baseball/\(gameID)/inning/\(inningID)") else { throw APIError.urlNotSupport }
+        return getAndDecode(url)
+    }
+    
     func requestScore() throws -> Observable<ScoreDTO> {
         guard let url = URL(string: "https://791e125f-f50f-4801-90d3-3b106e723a88.mock.pstmn.io/baseball/1/score") else {
-
             throw APIError.urlNotSupport
         }
         return getAndDecode(url)
     }
-
+    
+    
+    
     private func getAndDecode<T: Codable>(_ url: URL) -> Observable<T> {
         return Observable.create { observer in
             AF.request(url, method: .get)
@@ -40,8 +43,8 @@ class APIService {
             return Disposables.create()
         }
     }
+    
 }
-
 enum APIError: LocalizedError {
     case urlNotSupport
     case noData
