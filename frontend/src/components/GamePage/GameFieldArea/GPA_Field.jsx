8b75e50d61865689e6756cfa3842581bf96623f2
Seohyoung <prophecy803@gmail.com>
2021-05-13 16:07:47 +0900

    [#33] feat : log 화면에서 log context 및 dispatch 적용하여 데이터 수신 처리

--- frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
+++ frontend/src/components/GamePage/GameFieldArea/GPA_Field.jsx
@@ -1,89 +1,91 @@
+//GPA
 import React, { useState } from 'react';
-import styled, { css, keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { theme, Span } from '../../Style/Theme';
 import { ReactComponent as Field } from './Field.svg';
 import { ReactComponent as Ghost } from './ghost.svg';
 import API from '../../Hook/API';
-import { useGameState, useDispatch } from '../../GameContext';
+import {
+  useGameState,
+  useDispatch,
+  useLogState,
+  useLogDispatch,
+} from '../../GameContext';
 
-const run = keyframes`
-from {
-	transform: translateX(0rem) translateY(0rem);
-}
-to {
-	transform: translateX(15rem) translateY(-12.5rem);
-}`;
+const GPA_Field = ({ type, gameId }) => {
+  const { state } = useGameState();
+  const { logState } = useLogState();
+  const [move, setMove] = useState('');
+  const dispatch = useDispatch();
+  const logDispatch = useLogDispatch();
+  const GhostSVG = styled(Ghost)`
+    position: absolute;
+    width: 3rem;
+    top: 52.5rem;
+    left: 23rem;
+    animation: run 2s forwards;
+    ${move}
+  `;
 
-const GpaField = ({ type, gameId }) => {
-	const { state } = useGameState();
-	const [move, setMove] = useState(false);
-	const dispatch = useDispatch();
+  const handleClick = () => {
+    const pitchResult = async () => {
+      const response = await API.post.pitch(gameId);
+      dispatch({ type: 'pitch', payload: response });
+      logDispatch({ type: 'log', payload: response });
+    };
+    pitchResult();
 
-	const [inning, setInning] = useState(
-		state.score ? state.gameStatusDTO.inning : 1,
-	);
-	const handleClick = () => {
-		const pitchResult = async () => {
-			const response = await API.post.pitch(gameId);
-			dispatch({ type: 'pitch', payload: response });
-		};
-		pitchResult();
-		setMove(true);
-	};
+    setMove(`
+    @keyframes run {
+      from {
+        transform: translateX(0rem) translateY(0rem);
+      }
+      to {
+        transform: translateX(15rem) translateY(-12.5rem);
+      }
+    }`);
+  };
 
-	return (
-		<>
-			{type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
-			<FieldArea>
-				<GameState>{inning}회초 공격</GameState>
-				<FieldSVG />
-				<GhostSVG move={move} />
-			</FieldArea>
-		</>
-	);
+  return (
+    <>
+      {type === 'Attack' && <PITCH onClick={handleClick}>PITCH</PITCH>}
+      <FieldArea>
+        <GameState>2회초 공격</GameState>
+        <FieldSVG />
+        <GhostSVG />
+      </FieldArea>
+    </>
+  );
 };
 
-const GhostSVG = styled(Ghost)`
-	position: absolute;
-	width: 3rem;
-	top: 52.5rem;
-	left: 23rem;
-	animation: run 2s forwards;
-	${(props) =>
-		props.move &&
-		css`
-			animation-name: ${run};
-		`};
-`; //트랜지션
-
 const PITCH = styled.button`
-	position: absolute;
-	top: 23rem;
-	left: 3rem;
-	cursor: pointer;
-	z-index: 9999;
-	background-color: ${theme.colors.transparent};
-	font-size: ${theme.fontSize.XX_large};
-	font-weight: ${theme.fontWeight.Bold};
-	color: ${theme.colors.white};
-	border: 5px solid ${theme.colors.white};
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
+  cursor: pointer;
+  z-index: 9999;
+  background-color: ${theme.colors.transparent};
+  font-size: ${theme.fontSize.XX_large};
+  font-weight: ${theme.fontWeight.Bold};
+  color: ${theme.colors.white};
+  border: 5px solid ${theme.colors.white};
 `;
 const FieldArea = styled.div`
-	position: relative;
+  position: relative;
 `;
 
 const FieldSVG = styled(Field)`
-	position: absolute;
-	top: 23rem;
-	left: 3rem;
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
 `;
 
 const GameState = styled(Span)`
-	position: absolute;
-	top: 17rem;
-	right: 23rem;
-	font-size: ${theme.fontSize.X_large};
-	font-weight: ${theme.fontWeight.light};
-	color: ${theme.colors.white};
+  position: absolute;
+  top: 17rem;
+  right: 23rem;
+  font-size: ${theme.fontSize.X_large};
+  font-weight: ${theme.fontWeight.light};
+  color: ${theme.colors.white};
 `;
-export default GpaField;
+export default GPA_Field;
--- frontend/src/components/GamePage/GameLog.jsx
+++ frontend/src/components/GamePage/GameLog.jsx
@@ -1,92 +1,97 @@
+//gameLog
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { theme, Span } from '../Style/Theme';
 import Scroll from '../Style/Scroll';
-import { useGameState } from '../GameContext';
+import { useLogState } from '../GameContext';
 
 const GameLog = () => {
-	const { state } = useGameState();
-	const [logData, setLogData] = useState([]);
+  const { logState } = useLogState();
 
-	if (state.pitchResult) {
-		console.log(state.gameStatusDTO);
-	}
+  console.log(logState);
+  const SingleLog = () => {
+    return logState.map((log, i) => (
+      // console.log(log.pitchResult.playType, log.gameStatusDTO.strikeCount);
 
-	const SingleLog = () => {};
+      <Foo key={i}>
+        <span>{i}</span>
+        <span>{`${log.gameStatusDTO.currentHitter + 1}번 타자 ${
+          log.awayTeam.players[log.gameStatusDTO.currentHitter + 1].name
+        }`}</span>
+        <span>{log.pitchResult.playType}</span>
+        <span>{`S${log.gameStatusDTO.strikeCount} B${log.gameStatusDTO.ballCount}`}</span>
+      </Foo>
+    ));
+  };
 
-	useEffect(() => {
-		if (state.pitchResult) {
-			console.log(
-				state.gameStatusDTO.currentHitter,
-				state.pitchResult.playType,
-				`S${state.gameStatusDTO.strikeCount} B${state.gameStatusDTO.ballCount}`,
-			);
-		}
-	}, [logData]);
-
-	return (
-		<GameLogScroll>
-			<PlayerWrapper>
-				<Player now={true}>7번 타자 류진</Player>
-				<LogWrapper>
-					<Number>&#9316;</Number>
-					<Log>스트라이크</Log>
-					<AccLog>S2 B3</AccLog>
-				</LogWrapper>
-				<LogWrapper>
-					<Number>&#9315;</Number>
-					<Log>볼</Log>
-					<AccLog>S1 B3</AccLog>
-				</LogWrapper>
-				<LogWrapper>
-					<Number>&#9314;</Number>
-					<Log>볼</Log>
-					<AccLog>S1 B2</AccLog>
-				</LogWrapper>
-				<LogWrapper>
-					<Number>&#9313;</Number>
-					<Log>볼</Log>
-					<AccLog>S1 B1</AccLog>
-				</LogWrapper>
-				<LogWrapper>
-					<Number>&#9312;</Number>
-					<Log> 스트라이크</Log>
-					<AccLog>S1 B0</AccLog>
-				</LogWrapper>
-			</PlayerWrapper>
-		</GameLogScroll>
-	);
+  return (
+    <GameLogScroll>
+      <SingleLog />
+      <PlayerWrapper>
+        <Player now={true}>7번 타자 류진</Player>
+        <LogWrapper>
+          <Number>&#9316;</Number>
+          <Log>스트라이크</Log>
+          <AccLog>S2 B3</AccLog>
+        </LogWrapper>
+        <LogWrapper>
+          <Number>&#9315;</Number>
+          <Log>볼</Log>
+          <AccLog>S1 B3</AccLog>
+        </LogWrapper>
+        <LogWrapper>
+          <Number>&#9314;</Number>
+          <Log>볼</Log>
+          <AccLog>S1 B2</AccLog>
+        </LogWrapper>
+        <LogWrapper>
+          <Number>&#9313;</Number>
+          <Log>볼</Log>
+          <AccLog>S1 B1</AccLog>
+        </LogWrapper>
+        <LogWrapper>
+          <Number>&#9312;</Number>
+          <Log> 스트라이크</Log>
+          <AccLog>S1 B0</AccLog>
+        </LogWrapper>
+      </PlayerWrapper>
+    </GameLogScroll>
+  );
 };
 const GameLogScroll = styled(Scroll)``;
 
 const PlayerWrapper = styled.div`
-	padding-left: 20px;
+  padding-left: 20px;
 `;
 const LogWrapper = styled.div`
-	width: fit-content;
-	padding-top: 10px;
-	display: grid;
-	grid-template-columns: 20px 130px 100px;
+  width: fit-content;
+  padding-top: 10px;
+  display: grid;
+  grid-template-columns: 20px 130px 100px;
 `;
 const Player = styled.div`
-	margin: 15px 0;
-	font-size: ${theme.fontSize.medium};
-	font-weight: ${theme.fontWeight.medium};
-	color: ${(props) =>
-		props.now ? theme.colors.red_log : theme.colors.skyblue_log};
+  margin: 15px 0;
+  font-size: ${theme.fontSize.medium};
+  font-weight: ${theme.fontWeight.medium};
+  color: ${props =>
+    props.now ? theme.colors.red_log : theme.colors.skyblue_log};
 `;
 const Result = styled(Span)`
-	width: 150px;
-	color: ${theme.colors.blue_log};
+  width: 150px;
+  color: ${theme.colors.blue_log};
 `;
 const Number = styled(Span)`
-	color: ${theme.colors.white};
+  color: ${theme.colors.white};
 `;
 const Log = styled(Span)`
-	color: ${theme.colors.white};
+  color: ${theme.colors.white};
 `;
 const AccLog = styled(Span)`
-	color: ${theme.colors.grey};
+  color: ${theme.colors.grey};
+`;
+
+const Foo = styled(Span)`
+  color: ${theme.colors.white};
 `;
 
 export default GameLog;
