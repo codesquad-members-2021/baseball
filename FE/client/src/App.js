d09ab51a06b8c79cf7682d70e4760b4cf30c119c
jeonyeonkyu <ehb6915@naver.com>
2021-05-14 17:21:02 +0900

    Fix: 충돌 해결

--- FE/client/src/App.js
+++ FE/client/src/App.js
@@ -13,7 +13,6 @@ export const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
     }
   }
-
   body{
     font-family: 'Noto Sans KR';
     
@@ -24,38 +23,13 @@ export const GlobalStyle = createGlobalStyle`
   }
 `;
 
-return (
-  <Router>
+function App() {
+  return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/users">
-        <Users />
-      </Route>
-
+      <GlobalStyle />
+      <Page />
     </div>
-  </Router>
-);
-
+  );
+}
 
 export default App;
