a3a488c865a6c49b0fc4f5b7b0fe05cdfd8d7d19
Polynomeer <polynomeer@gmail.com>
2021-05-14 12:41:31 +0900

    chore: Add dummy data to data.sql (#45)
    Added additional dummy data for games and teams.

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -6,6 +6,21 @@ INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Lotte Giants', '1'
 INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Doosan Bears', '0');
 INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('SSG Landers', '0');
 INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('LG Twins', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Atlanta Braves', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Boston Red Sox', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Chicago Cubs', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Cleveland Indians', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Houston Astros', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Los Angeles Angels', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Los Angeles Dodgers', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('New York Mets', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('New York Yankees', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Philadelphia Phillies', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('San Diego Padres', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('San Francisco Giants', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('St. Louis Cardinals', '0');
+INSERT INTO `baseball`.`team` (`name`, `is_playing`) VALUES ('Toronto Blue Jays', '0');
+
 
 INSERT INTO `baseball`.`player` (`id`, `number`, `name`, `position`, `team_name`) VALUES ('1', '1', '강민호', 'pitcher', 'Samsung Lions');
 INSERT INTO `baseball`.`player` (`id`, `number`, `name`, `position`, `team_name`) VALUES ('2', '2', '강한울', 'hitter', 'Samsung Lions');
@@ -33,3 +48,11 @@ INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home
 INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('2', '0', '0', '0', 'NC Dinos', 'Hanwha Eagles');
 INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('3', '0', '0', '0', 'Lotte Giants', 'Doosan Bears');
 INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('4', '0', '0', '0', 'SSG Landers', 'LG Twins');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('5', '0', '0', '0', 'New York Yankees', 'New York Mets');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('6', '0', '0', '0', 'Los Angeles Dodgers', 'Boston Red Sox');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('7', '0', '0', '0', 'Atlanta Braves', 'Los Angeles Angels');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('8', '0', '0', '0', 'St. Louis Cardinals', 'Chicago Cubs');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('9', '0', '0', '0', 'Toronto Blue Jays', 'San Francisco Giants');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('10', '0', '0', '0', 'Houston Astros', 'San Diego Padres');
+INSERT INTO `baseball`.`game` (`id`, `home_score`, `away_score`, `is_end`, `home_name`, `away_name`) VALUES ('11', '0', '0', '0', 'Philadelphia Phillies', 'Cleveland Indians');
+
