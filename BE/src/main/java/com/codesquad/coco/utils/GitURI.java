d5113bcff003557f015dd97abb13d4e61aa9988a
gisungchoi <chlrltjd159@naver.com>
2021-05-14 23:09:38 +0900

    feat : 3000 포트는 cors 열어주기
    - 특정 포트의 cors 요청은 허용한다

--- BE/src/main/java/com/codesquad/coco/utils/GitURI.java
+++ BE/src/main/java/com/codesquad/coco/utils/GitURI.java
@@ -2,7 +2,7 @@ package com.codesquad.coco.utils;
 
 public enum GitURI {
 
-    REDIRECT_URI("http://localhost:8080/login/github"),
+    REDIRECT_URI("http://ec2-52-78-180-217.ap-northeast-2.compute.amazonaws.com/login/github"),
     ACCESS_TOKEN_URI("https://github.com/login/oauth/access_token"),
     AUTHORIZE_URI("https://github.com/login/oauth/authorize"),
     USER_INFO_URI("https://api.github.com/user");
