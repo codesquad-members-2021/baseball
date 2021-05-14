884bc1cd85457a7f2fa6af5ca4b4a8c3afc705c4
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-05-14 15:52:19 +0900

    Feat: remove average field considered to be useless

--- BE/src/main/java/baseball/domain/Record.java
+++ BE/src/main/java/baseball/domain/Record.java
@@ -5,20 +5,11 @@ public class Record {
     private int atBat;
     private int hit;
     private int out;
-    private double average;
 
     public Record(int atBat, int hit, int out) {
         this.atBat = atBat;
         this.hit = hit;
         this.out = out;
-        this.average = calculateAverage(atBat, hit);
-    }
-
-    private double calculateAverage(int atBat, int hit) {
-        if (hit == 0) {
-            return 0;
-        }
-        return atBat / (double) hit;
     }
 
     public int getAtBat() {
@@ -31,9 +22,5 @@ public class Record {
 
     public int getOut() {
         return out;
-    }
-
-    public double getAverage() {
-        return average;
     }
 }
