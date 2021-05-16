bfff8977e6a2713e8b9288ccdad9b6af5961d2f2
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-05-16 11:58:20 +0900

    docs : 애노테이션을 설명하는 주석 추가

--- backend/src/main/java/com/codesquad/baseball/annotation/Auth.java
+++ backend/src/main/java/com/codesquad/baseball/annotation/Auth.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/*
+* Auth 애노테이션은 컨트롤러 메서드에 붙이는 애노테이션입니다
+* Auth 어노테이션이 달려있으면 인터셉터에서 액세스 토큰을 검사합니다
+* */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
