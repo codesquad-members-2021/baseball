548cb08e84ac3c99685463f2d1ef3ff74d5ca253
정이삭 <isaac56@naver.com>
2021-05-18 15:52:36 +0900

    feat: Remove null property from json response by using @JsonInclude

--- BE/baseball/src/main/java/team9/baseball/DTO/response/ApiResult.java
+++ BE/baseball/src/main/java/team9/baseball/DTO/response/ApiResult.java
@@ -1,10 +1,12 @@
 package team9.baseball.DTO.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResult<T> {
     private T data;
     private String error;
